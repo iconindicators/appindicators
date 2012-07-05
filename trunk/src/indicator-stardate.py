3b804418d90a8aaca789a1f8e7650a9e4a9dcbec
bernmeister <thebernmeister@hotmail.com>
2012-07-05 04:59:02 +0000

    Now include example.py in the release. Increased the version number for the next eventual release.

--- trunk/src/indicator-stardate.py
+++ trunk/src/indicator-stardate.py
@@ -39,7 +39,7 @@ class IndicatorStardate:
 
     AUTHOR = "Bernard Giannetti"
     NAME = "indicator-stardate"
-    VERSION = "1.0.4"
+    VERSION = "1.0.5"
     ICON = "indicator-stardate"
 
     AUTOSTART_PATH = os.getenv( "HOME" ) + "/.config/autostart/" + NAME + ".desktop"
