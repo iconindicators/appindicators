8022713a4b7e1bc8a27edac98974101fd315d2b1
bernmeister <thebernmeister@hotmail.com>
2012-09-28 00:04:55 +0000

    Changes for Python3 compatibility.

--- trunk/src/example.py
+++ trunk/src/example.py
@@ -21,13 +21,13 @@ import stardate
 
 currentStardate = stardate.Stardate()
 
-print "Stardate API version: " + currentStardate.getVersion()
+print( "Stardate API version: " + currentStardate.getVersion() )
 
 stardateIssue = -28
 stardateInteger = 6857
 stardateFraction = 59
 currentStardate.setStardate( stardateIssue, stardateInteger, stardateFraction )
-print "Stardate [" + str( stardateIssue ) + "] " + str( stardateInteger ) + "." + str( stardateFraction ) + " = " + currentStardate.toGregorianString()
+print( "Stardate [" + str( stardateIssue ) + "] " + str( stardateInteger ) + "." + str( stardateFraction ) + " = " + currentStardate.toGregorianString() )
 
 gregorianYear = 2012
 gregorianMonth = 6
@@ -36,4 +36,4 @@ gregorianHour = 16
 gregorianMinute = 49
 gregorianSecond = 20
 currentStardate.setGregorian( datetime.datetime( gregorianYear, gregorianMonth, gregorianDay, gregorianHour, gregorianMinute, gregorianSecond ) )
-print "Gregorian " + str( gregorianYear ) + "/" + str( gregorianMonth ) + "/" + str( gregorianDay ) + " " + str( gregorianHour ) + ":" + str( gregorianMinute ) + ":" + str( gregorianSecond ) + " (y/m/d h:m:s) = " + currentStardate.toStardateString( False, True )
+print( "Gregorian " + str( gregorianYear ) + "/" + str( gregorianMonth ) + "/" + str( gregorianDay ) + " " + str( gregorianHour ) + ":" + str( gregorianMinute ) + ":" + str( gregorianSecond ) + " (y/m/d h:m:s) = " + currentStardate.toStardateString( False, True ) )
