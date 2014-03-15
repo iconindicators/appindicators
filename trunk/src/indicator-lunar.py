37f4a7e4acbed096665bde9d8b0aae9dc6eb9193
bernmeister <thebernmeister@hotmail.com>
2014-03-15 01:43:25 +0000

    Update for new release.

--- trunk/src/indicator-lunar.py
+++ trunk/src/indicator-lunar.py
@@ -55,7 +55,7 @@ class IndicatorLunar:
 
     AUTHOR = "Bernard Giannetti"
     NAME = "indicator-lunar"
-    VERSION = "1.0.37"
+    VERSION = "1.0.38"
     ICON = NAME
     LOG = os.getenv( "HOME" ) + "/" + NAME + ".log"
     WEBSITE = "https://launchpad.net/~thebernmeister"
