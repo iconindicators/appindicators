893ef69465f70e9da56e39b786742ac433f790b4
bernmeister <thebernmeister@hotmail.com>
2014-03-15 01:43:24 +0000

    Update for new release.

--- trunk/src/indicator-fortune.py
+++ trunk/src/indicator-fortune.py
@@ -38,7 +38,7 @@ class IndicatorFortune:
 
     AUTHOR = "Bernard Giannetti"
     NAME = "indicator-fortune"
-    VERSION = "1.0.8"
+    VERSION = "1.0.9"
     ICON = NAME
     LOG = os.getenv( "HOME" ) + "/" + NAME + ".log"
     WEBSITE = "https://launchpad.net/~thebernmeister"
