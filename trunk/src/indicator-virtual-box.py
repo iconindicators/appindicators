5d456bae7910d7e650989f14482545e8e958877d
bernmeister <thebernmeister@hotmail.com>
2014-03-15 01:43:23 +0000

    Update for new release.

--- trunk/src/indicator-virtual-box.py
+++ trunk/src/indicator-virtual-box.py
@@ -41,7 +41,7 @@ class IndicatorVirtualBox:
 
     AUTHOR = "Bernard Giannetti"
     NAME = "indicator-virtual-box"
-    VERSION = "1.0.30"
+    VERSION = "1.0.31"
     ICON = NAME
     LOG = os.getenv( "HOME" ) + "/" + NAME + ".log"
     WEBSITE = "https://launchpad.net/~thebernmeister"
