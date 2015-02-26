805970aaa5a66c85f3d4afaa32af18e828e1f90c
bernmeister <thebernmeister@hotmail.com>
2015-02-26 05:01:56 +0000

    Clean up of Celestrak test script.

--- trunk/src/celestrak.py
+++ trunk/src/celestrak.py
@@ -1,72 +1,84 @@
 #!/usr/bin/env python3
 
 
-# Test file to determine how many of the satellites et al from Celestrak (visual, weather, ... all)
-# are rocket booster, debris, platform, payload or unknown by looking at the full text database from Celestrak.
-# Requires downloading the full text database (satcat).
-# Requires all the TLE files for all satellites, etc and combined (cat) into one file.
+# Determine how many of the satellites et al from Celestrak
+# are rocket booster, debris, platform, payload or unknown
+# by looking at the full text database from Celestrak.
 
 
 from urllib.request import urlopen
-import satellite
+
+
+def getSatCat( url ):
+    satCat = { } # Key: Norad number; Value: data line.
+    data = urlopen( url ).read().decode( "utf8" ).splitlines()
+    for line in data:
+        noradNumber = line[ 13 : 18 ]
+        satCat[ noradNumber ] = line
+
+    return satCat
 
 
 def getSatelliteTLEData( url ):
     satelliteTLEData = { } # Key: ( satellite name, satellite number ) ; Value: satellite.TLE object.
     data = urlopen( url ).read().decode( "utf8" ).splitlines()
     for i in range( 0, len( data ), 3 ):
-        tle = satellite.TLE( data[ i ].strip(), data[ i + 1 ].strip(), data[ i + 2 ].strip() )
-        satelliteTLEData[ ( tle.getName(), tle.getNumber() ) ] = tle
+        title = data[ i ].strip()
+        line1 = data[ i + 1 ].strip()
+        line2 = data[ i + 2 ].strip()
+        satelliteTLEData[ title ] = [ line1, line2 ]
 
     return satelliteTLEData
 
 
-def getSatCat():
-    satCat = { } # Key: Norad number; Value: data line.
-    f = open( "/home/bernard/Desktop/satcat.txt", "r" )
-    for line in f:
-        noradNumber = line[ 13 : 18 ]
-        satCat[ noradNumber ] = line
-        
-    return satCat
 
-
-satCat = getSatCat()
-# satelliteTLEData = getSatelliteTLEData( "http://celestrak.com/NORAD/elements/visual.txt" )
-satelliteTLEData = getSatelliteTLEData( "file:///home/bernard/Desktop/celestrak.txt" )
+satCat = getSatCat( "http://celestrak.com/pub/satcat.txt" )
+satelliteTLEData = getSatelliteTLEData( "http://celestrak.com/NORAD/elements/visual.txt" )
 unknowns = [ ]
-rb = 0
-deb = 0
-plat = 0
-pay = 0
+rb = [ ]
+deb = [ ]
+plat = [ ]
+pay = [ ]
 for key in satelliteTLEData:
-    name = satelliteTLEData.get( key ).getName()
-    number = satelliteTLEData.get( key ).getNumber()
-    internationalDesignator = satelliteTLEData.get( key ).getInternationalDesignator()
-    
+    title = key
+    line1 = satelliteTLEData[ key ][ 0 ]
+    line2 = satelliteTLEData[ key ][ 1 ]
+
+    launchYear = line1[ 9 : 11 ]
+    if int( launchYear ) < 57:
+        launchYear = "20" + launchYear
+    else:
+        launchYear = "19" + launchYear 
+
+    launchYear + "-" + line1[ 11 : 17 ].strip()
+
+    name = title
+    number = line1[ 2 : 7 ]
+    internationalDesignator = launchYear
+
     if "R/B" in name.upper():
-        rb += 1
+        rb.append( name )
         continue
 
     if "DEB" in name.upper():
-        deb += 1
+        deb.append( name )
         continue
 
     if "PLAT" in name.upper():
-        plat += 1
+        plat.append( name )
         continue
 
     payload = satCat[ number ][ 20 ]
     if "*" in payload:
-        pay += 1
+        pay.append( name )
         continue
 
     unknowns.append( name )
 
-print( "SatCat size", len( satCat ) )
-print( "Master size", len( satelliteTLEData ) )
-print( "R/B", rb )
-print( "DEB", deb )
-print( "PLAT", plat )
-print( "PAYLOAD", pay )
-print( "Unknowns", unknowns )
+print( "SatCat size:", len( satCat ) )
+print( "TLE data size:", len( satelliteTLEData ) )
+print( "R/B:", len( rb ), sorted( rb ) )
+print( "DEB:", len( deb ), sorted( deb ) )
+print( "PLAT:", len( plat ), sorted( plat ) )
+print( "PAYLOAD:", len( pay ), sorted( pay ) )
+print( "Unknowns:", len( unknowns ), sorted( unknowns ) )
