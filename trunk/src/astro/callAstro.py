150ef589cce429ddf380c58687b78d87f6b83040
bernmeister <thebernmeister@hotmail.com>
2015-05-22 04:09:04 +0000

    Test stuff for possible split of astro stuff in a separate file.

---
+++ trunk/src/astro/callAstro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+import pythonutils
+
+
+pythonutils.processCall( \
+    "python3 astro.py " + \
+    " ' -33:52:04' " + \
+    "' 151:12:26' " + \
+    "58 " + \
+    "'1984-05-30 16:23:45'" ) #TODO Needs to be new format.
