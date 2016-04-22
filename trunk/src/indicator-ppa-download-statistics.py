bc5d15bb5e088f3e91201c6158547b506b07412a
bernmeister <thebernmeister@hotmail.com>
2016-04-22 00:51:03 +0000

    Added yakkety to list of series.

--- trunk/src/indicator-ppa-download-statistics.py
+++ trunk/src/indicator-ppa-download-statistics.py
@@ -56,7 +56,7 @@ class IndicatorPPADownloadStatistics:
 
     DESKTOP_FILE = INDICATOR_NAME + ".desktop"
 
-    SERIES = [ "xenial", "wily", "vivid", "utopic", "trusty", "saucy", "raring", "quantal", "precise", "oneiric", "natty", "maverick", "lucid", "karmic", "jaunty", "intrepid", "hardy", "gutsy", "feisty", "edgy", "dapper", "breezy", "hoary", "warty" ]
+    SERIES = [ "yakkety", "xenial", "wily", "vivid", "utopic", "trusty", "saucy", "raring", "quantal", "precise", "oneiric", "natty", "maverick", "lucid", "karmic", "jaunty", "intrepid", "hardy", "gutsy", "feisty", "edgy", "dapper", "breezy", "hoary", "warty" ]
     ARCHITECTURES = [ "amd64", "i386" ]
 
     COMMENTS = _( "Shows the total downloads of PPAs." )
