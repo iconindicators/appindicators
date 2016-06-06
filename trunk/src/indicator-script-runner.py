9f25594d6bb4aa402364e743d34e19b60ff75b01
bernmeister <thebernmeister@hotmail.com>
2016-06-06 12:54:14 +0000

    Updated TODO.

--- trunk/src/indicator-script-runner.py
+++ trunk/src/indicator-script-runner.py
@@ -24,6 +24,7 @@
 
 
 #TODO Can't run multiple scripts simultaneously (test with both true and false values for terminalOpen).
+# IF we allow this to happen, the prefs needs to use the backup copy of the scripts to modify...that means passing in the scripts lists to all functions...big change!
 
 
 #TODO Do a deb build (of all indicators) and look at the w/e from the Lintian output.
