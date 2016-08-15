b32df67f8f12c896287e765a5acf928a9b78d2df
bernmeister <thebernmeister@hotmail.com>
2016-08-15 23:59:26 +0000

    Added utf-8 encoding line. Fixed PyGIWarnings on imports.

--- trunk/src/indicator-virtual-box.py
+++ trunk/src/indicator-virtual-box.py
@@ -34,6 +34,9 @@ INDICATOR_NAME = "indicator-virtual-box"
 import gettext
 gettext.install( INDICATOR_NAME )
 
+import gi
+gi.require_version( "AppIndicator3", "0.1")
+
 from gi.repository import AppIndicator3, GLib, Gtk
 from threading import Thread
 
