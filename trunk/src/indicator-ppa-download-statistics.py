f5da27e7276d8bb05fe3d1830fdc274308eabdd5
bernmeister <thebernmeister@hotmail.com>
2016-08-24 04:12:39 +0000

    Bug fix: Now uses the X-GNOME-Autostart-enabled tag for autostart.

--- trunk/src/indicator-ppa-download-statistics.py
+++ trunk/src/indicator-ppa-download-statistics.py
@@ -59,7 +59,7 @@ class IndicatorPPADownloadStatistics:
     LOG = os.getenv( "HOME" ) + "/" + INDICATOR_NAME + ".log"
     WEBSITE = "https://launchpad.net/~thebernmeister"
 
-    DESKTOP_FILE = INDICATOR_NAME + ".desktop"
+    DESKTOP_FILE = INDICATOR_NAME + ".py.desktop"
 
     SERIES = [ "yakkety", "xenial", "wily", "vivid", "utopic", "trusty", "saucy", "raring", "quantal", "precise", "oneiric", "natty", "maverick", "lucid", "karmic", "jaunty", "intrepid", "hardy", "gutsy", "feisty", "edgy", "dapper", "breezy", "hoary", "warty" ]
     ARCHITECTURES = [ "amd64", "i386" ]
@@ -522,7 +522,7 @@ class IndicatorPPADownloadStatistics:
         grid.attach( allowMenuItemsToLaunchBrowserCheckbox, 0, 6, 2, 1 )
 
         autostartCheckbox = Gtk.CheckButton( _( "Autostart" ) )
-        autostartCheckbox.set_active( pythonutils.isAutoStart( IndicatorPPADownloadStatistics.DESKTOP_FILE ) )
+        autostartCheckbox.set_active( pythonutils.isAutoStart( IndicatorPPADownloadStatistics.DESKTOP_FILE ), logging )
         autostartCheckbox.set_tooltip_text( _( "Run the indicator automatically." ) )
         autostartCheckbox.set_margin_top( 10 )
         grid.attach( autostartCheckbox, 0, 7, 2, 1 )
