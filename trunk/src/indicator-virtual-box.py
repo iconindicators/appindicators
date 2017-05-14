da3695a2a5cde29c7351949e8ca267c363713322
bernmeister <thebernmeister@hotmail.com>
2017-05-14 09:44:45 +0000

    Added _ to internal function names.

--- trunk/src/indicator-virtual-box.py
+++ trunk/src/indicator-virtual-box.py
@@ -472,11 +472,11 @@ class IndicatorVirtualBox:
 
     def onPreferences( self, widget ):
         if self.lock.acquire( blocking = False ):
-            self.onPreferencesInternal( widget )
+            self._onPreferencesInternal( widget )
             self.lock.release()
 
 
-    def onPreferencesInternal( self, widget ):
+    def _onPreferencesInternal( self, widget ):
         notebook = Gtk.Notebook()
 
         # List of VMs.
