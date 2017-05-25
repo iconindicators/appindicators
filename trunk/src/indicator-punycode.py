79cb6b958bc57411e2b1a328fefa183239968a82
bernmeister <thebernmeister@hotmail.com>
2017-05-25 01:50:11 +0000

    Using new method to initialse logging.

--- trunk/src/indicator-punycode.py
+++ trunk/src/indicator-punycode.py
@@ -61,8 +61,7 @@ class IndicatorPunycode:
 
 
     def __init__( self ):
-        filehandler = pythonutils.TruncatedFileHandler( IndicatorPunycode.LOG, "a", 10000, None, True )
-        logging.basicConfig( format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s", level = logging.DEBUG, handlers = [ filehandler ] )
+        logging.basicConfig( format = pythonutils.LOGGING_BASIC_CONFIG_FORMAT, level = pythonutils.LOGGING_BASIC_CONFIG_LEVEL, handlers = [ pythonutils.TruncatedFileHandler( IndicatorPunycode.LOG ) ] )
         self.dialogLock = threading.Lock()
         self.results =  [ ] # List of lists, each sublist contains [ unicode, ascii ].
 
