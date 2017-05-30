c0787b87cd86925512076b8022251ce421a764f6
bernmeister <thebernmeister@hotmail.com>
2017-05-30 02:25:39 +0000

    Rename indent contants.

--- trunk/src/indicator-fortune.py
+++ trunk/src/indicator-fortune.py
@@ -332,17 +332,17 @@ class IndicatorFortune:
 
         radioMiddleMouseClickNewFortune = Gtk.RadioButton.new_with_label_from_widget( None, _( "Show a new fortune" ) )
         radioMiddleMouseClickNewFortune.set_active( self.middleMouseClickOnIcon == IndicatorFortune.SETTINGS_MIDDLE_MOUSE_CLICK_ON_ICON_NEW )
-        radioMiddleMouseClickNewFortune.set_margin_left( pythonutils.WIDGET_INDENT_LEFT )
+        radioMiddleMouseClickNewFortune.set_margin_left( pythonutils.INDENT_WIDGET_LEFT )
         grid.attach( radioMiddleMouseClickNewFortune, 0, 4, 1, 1 )
 
         radioMiddleMouseClickCopyLastFortune = Gtk.RadioButton.new_with_label_from_widget( radioMiddleMouseClickNewFortune, _( "Copy current fortune to clipboard" ) )
         radioMiddleMouseClickCopyLastFortune.set_active( self.middleMouseClickOnIcon == IndicatorFortune.SETTINGS_MIDDLE_MOUSE_CLICK_ON_ICON_COPY_LAST )
-        radioMiddleMouseClickCopyLastFortune.set_margin_left( pythonutils.WIDGET_INDENT_LEFT )
+        radioMiddleMouseClickCopyLastFortune.set_margin_left( pythonutils.INDENT_WIDGET_LEFT )
         grid.attach( radioMiddleMouseClickCopyLastFortune, 0, 5, 1, 1 )
 
         radioMiddleMouseClickShowLastFortune = Gtk.RadioButton.new_with_label_from_widget( radioMiddleMouseClickNewFortune, _( "Show current fortune" ) )
         radioMiddleMouseClickShowLastFortune.set_active( self.middleMouseClickOnIcon == IndicatorFortune.SETTINGS_MIDDLE_MOUSE_CLICK_ON_ICON_SHOW_LAST )
-        radioMiddleMouseClickShowLastFortune.set_margin_left( pythonutils.WIDGET_INDENT_LEFT )
+        radioMiddleMouseClickShowLastFortune.set_margin_left( pythonutils.INDENT_WIDGET_LEFT )
         grid.attach( radioMiddleMouseClickShowLastFortune, 0, 6, 1, 1 )
 
         autostartCheckbox = Gtk.CheckButton( _( "Autostart" ) )
