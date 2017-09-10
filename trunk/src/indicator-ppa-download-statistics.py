9b5ea2996da82d627fe9116a8489bcf308ec5ea7
bernmeister <thebernmeister@hotmail.com>
2017-09-10 06:11:58 +0000

    Combine two loops for download and download on error.

--- trunk/src/indicator-ppa-download-statistics.py
+++ trunk/src/indicator-ppa-download-statistics.py
@@ -1023,11 +1023,9 @@ class IndicatorPPADownloadStatistics:
             else:
                 self.getPublishedBinariesNoFilters( ppa )
 
-        # Have a second attempt at failures...
-        for ppa in self.ppas:
+            # Try again on failure...
             if ppa.getStatus() == PPA.STATUS_ERROR_RETRIEVING_PPA:
                 ppa.setStatus( PPA.STATUS_NEEDS_DOWNLOAD )
-                key = ppa.getUser() + " | " + ppa.getName()
                 if key in self.filters:
                     self.getPublishedBinariesWithFilters( ppa )
                 else:
