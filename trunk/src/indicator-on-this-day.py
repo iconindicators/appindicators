9bb84bd153b53cf4f7a0d58a9bbb4bff87d25295
bernmeister <thebernmeister@hotmail.com>
2017-10-06 01:09:15 +0000

    Bug fix: In Ubuntu 16.04 and greater, icons for ubuntu-mono-dark and ubuntu-mono-light would not load due to directories not present in the underlying index.theme file.

--- trunk/src/indicator-on-this-day.py
+++ trunk/src/indicator-on-this-day.py
@@ -48,7 +48,7 @@ import fnmatch, logging, os, pythonutils, threading, webbrowser
 class IndicatorOnThisDay:
 
     AUTHOR = "Bernard Giannetti"
-    VERSION = "1.0.0"
+    VERSION = "1.0.1"
     ICON = INDICATOR_NAME
     DESKTOP_FILE = INDICATOR_NAME + ".py.desktop"
     LOG = os.getenv( "HOME" ) + "/" + INDICATOR_NAME + ".log"
