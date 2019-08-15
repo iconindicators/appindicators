e9ad458b1d4f0213806a9a4464728d328f5ed842
bernmeister <thebernmeister@hotmail.com>
2019-08-15 07:00:22 +0000

    In progress...

--- trunk/src/astroTest.py
+++ trunk/src/astroTest.py
@@ -5,36 +5,36 @@
 import astroPyephem, astroSkyfield, datetime
 
 
-def compareResults( resultsPyephem, resultsSkyfield ):
+def compareResults( resultsPyephem, resultsSkyfield, astronomicalBodyType, nameTag, dataTagsPyephem, dataTagsSkyfield ):
 
     results = [ ]
-    for dataTag in astroPyephem.DATA_MOON:
-        if ( astroPyephem.AstronomicalBodyType.Moon, astroPyephem.NAME_TAG_MOON, dataTag ) in resultsPyephem and \
-           ( astroPyephem.AstronomicalBodyType.Moon, astroPyephem.NAME_TAG_MOON, dataTag ) not in resultsSkyfield:
+    for dataTag in dataTagsPyephem:
+        if ( astronomicalBodyType, nameTag, dataTag ) in resultsPyephem and \
+           ( astronomicalBodyType, nameTag, dataTag ) not in resultsSkyfield:
             results.append( dataTag )
 
     if results:
-        print( "Moon keys in Pyephem not in Skyfield:", results )
+        print( nameTag, "keys in Pyephem not in Skyfield:", results )
 
     results = [ ]
-    for dataTag in astroSkyfield.DATA_MOON:
-        if ( astroSkyfield.AstronomicalBodyType.Moon, astroSkyfield.NAME_TAG_MOON, dataTag ) in resultsSkyfield and \
-           ( astroSkyfield.AstronomicalBodyType.Moon, astroSkyfield.NAME_TAG_MOON, dataTag ) not in resultsPyephem:
+    for dataTag in dataTagsSkyfield:
+        if ( astronomicalBodyType, nameTag, dataTag ) in resultsSkyfield and \
+           ( astronomicalBodyType, nameTag, dataTag ) not in resultsPyephem:
             results.append( dataTag )
 
     if results:
-        print( "Moon keys in Skyfield not in Pyephem:", results )
+        print( nameTag, "keys in Skyfield not in Pyephem:", results )
 
     results = [ ]
-    for dataTag in astroPyephem.DATA_MOON:
-        key = ( astroPyephem.AstronomicalBodyType.Moon, astroPyephem.NAME_TAG_MOON, dataTag )
+    for dataTag in dataTagsPyephem:
+        key = ( astronomicalBodyType, nameTag, dataTag )
         if key in resultsPyephem and key in resultsSkyfield and resultsPyephem[ key ] != resultsSkyfield[ key ]:
             results.append( dataTag )
 
     if results:
-        print( "Moon values in Pyephem which differ from Skyfield:" )
+        print( nameTag, "values in Pyephem which differ from Skyfield:" )
         for dataTag in results:
-            key = ( astroPyephem.AstronomicalBodyType.Moon, astroPyephem.NAME_TAG_MOON, dataTag )
+            key = ( astronomicalBodyType, nameTag, dataTag )
             print( "\t", dataTag, resultsPyephem[ key ], resultsSkyfield[ key ] )
 
 
@@ -65,4 +65,5 @@ resultsPyephem = astroPyephem.getAstronomicalInformation( utcNow,
                                                           magnitude )
 
 print( "Crunching results..." )
-compareResults( resultsPyephem, resultsSkyfield )
+compareResults( resultsPyephem, resultsSkyfield, astroSkyfield.AstronomicalBodyType.Moon, astroSkyfield.NAME_TAG_MOON, astroPyephem.DATA_MOON, astroSkyfield.DATA_MOON )
+compareResults( resultsPyephem, resultsSkyfield, astroSkyfield.AstronomicalBodyType.Sun, astroSkyfield.NAME_TAG_SUN, astroPyephem.DATA_SUN, astroSkyfield.DATA_SUN )
