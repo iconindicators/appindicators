77d60ca5210ba9b6b205f5f47afc7b5ec2f2e505
bernmeister <thebernmeister@hotmail.com>
2019-09-17 02:26:46 +0000

    Corrected tags for comets and minor planets to be aligned with other bodies.

--- trunk/src/astroSkyfield.py
+++ trunk/src/astroSkyfield.py
@@ -65,15 +65,15 @@ DATA_INTERNAL = [
 
 #TODO Are these sub-lists needed?  Might be needed for the Preferences dialgo.
 DATA_COMET = [
-    DATA_RISE_AZIMUTH,
+    DATA_ALTITUDE,
+    DATA_AZIMUTH,
     DATA_RISE_DATE_TIME,
-    DATA_SET_AZIMUTH,
     DATA_SET_DATE_TIME ]
 
 DATA_MINOR_PLANET = [
-    DATA_RISE_AZIMUTH,
+    DATA_ALTITUDE,
+    DATA_AZIMUTH,
     DATA_RISE_DATE_TIME,
-    DATA_SET_AZIMUTH,
     DATA_SET_DATE_TIME ]
 
 DATA_MOON = [
