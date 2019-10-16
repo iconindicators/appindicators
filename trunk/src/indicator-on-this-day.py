fbaaf7add2d843daf0135164a4bfa3536a3b4e78
bernmeister <thebernmeister@hotmail.com>
2019-10-16 23:07:47 +0000

    Uses base version of autostart checkbox.

--- trunk/src/indicator-on-this-day.py
+++ trunk/src/indicator-on-this-day.py
@@ -291,10 +291,7 @@ class IndicatorOnThisDay( indicatorbase.IndicatorBase ):
         notifyCheckbox.set_margin_top( 10 )
         grid.attach( notifyCheckbox, 0, 5, 1, 1 )
 
-        autostartCheckbox = Gtk.CheckButton( _( "Autostart" ) )
-        autostartCheckbox.set_tooltip_text( _( "Run the indicator automatically." ) )
-        autostartCheckbox.set_active( self.isAutoStart() )
-        autostartCheckbox.set_margin_top( 10 )
+        autostartCheckbox = self.createAutostartCheckbox() 
         grid.attach( autostartCheckbox, 0, 6, 1, 1 )
 
         notebook.append_page( grid, Gtk.Label( _( "General" ) ) )
