cf6709e7884bab52f5d61be99c6e4056ee8a6d89
bernmeister <thebernmeister@hotmail.com>
2020-01-17 04:51:07 +0000

    Checks version number in changelog/source with PPA.

---
+++ trunk/src/checkUnreleased.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# Iterate through each indicator's main file and changelog, 
+# compare the version numbers and report any difference.
+
+
+basePath = "../../"
+changeLogPath = "/packaging/debian/changelog"
+sourcePath = "/src/"
+indicators = [
+    [ "IndicatorFortune", "indicator-fortune", "1.0.30" ],
+    [ "IndicatorLunar", "indicator-lunar-NEW", "1.0.80" ], #TODO Fix when released!
+    [ "IndicatorOnThisDay", "indicator-on-this-day", "1.0.6" ],
+    [ "IndicatorPPADownloadStatistics", "indicator-ppa-download-statistics", "1.0.67" ],
+    [ "IndicatorPunycode", "indicator-punycode", "1.0.8" ],
+    [ "IndicatorScriptRunner", "indicator-script-runner", "1.0.11" ],
+    [ "IndicatorStardate", "indicator-stardate", "1.0.38" ],
+    [ "IndicatorTide", "indicator-tide", "1.0.19" ],
+    [ "IndicatorVirtualBox", "indicator-virtual-box", "1.0.62" ] ]
+
+
+for project, source, versionOnPPA in indicators:
+    with open( basePath + project + changeLogPath ) as f:
+        content = f.readlines()
+        for line in content:
+            versionChangeLog = line[ line.index( '(' ) + 1 : line.index( ')' ) - 2 ]
+            break
+ 
+    with open( basePath + project + sourcePath + source + ".py" ) as f:
+        content = f.readlines()
+        for line in content:
+            if "version =" in line:
+                versionSource = line[ line.index( '"' ) + 1 : line.index( ',' ) - 1 ]
+                break
+
+    if versionChangeLog != versionSource or versionChangeLog != versionOnPPA:
+        print( project, "has version mismatch:" )
+        print( "\tPPA:", versionOnPPA )
+        print( "\tChangeLog:", versionChangeLog )
+        print( "\tSource", versionSource )
