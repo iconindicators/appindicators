2788f04bc65699d21a7691fefe3b290150826741
bernmeister <thebernmeister@hotmail.com>
2020-04-09 12:57:48 +0000

    More cache/update.

--- trunk/src/indicator-lunar.py
+++ trunk/src/indicator-lunar.py
@@ -392,7 +392,7 @@ class IndicatorLunar( indicatorbase.IndicatorBase ):
                     pass
                     #TODO No data from the download...so set the next download time to what?
 #                     nextDownloadTime = datetime.datetime.utcnow() + datetime.timedelta( minutes = self.getNextDownloadInterval( downloadCount ) )
-#                     cacheDateTime = ???
+                    cacheDateTime = datetime.datetime.utcnow() - datetime.timedelta( hours = 1000 ) #TODO If this is correct...write a comment!
 
         return data, cacheDateTime, downloadCount, nextDownloadTime
 
