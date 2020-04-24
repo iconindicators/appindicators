ed1b8264f29d855537ea0a420da1c0803b6b7bea
bernmeister <thebernmeister@hotmail.com>
2020-04-24 13:31:00 +0000

    Updated to latest versions.

--- trunk/src/checkUnreleased.py
+++ trunk/src/checkUnreleased.py
@@ -11,15 +11,15 @@ basePath = "../../"
 changeLogPath = "/packaging/debian/changelog"
 sourcePath = "/src/"
 indicators = [
-    [ "IndicatorFortune", "indicator-fortune", "1.0.30" ],
-    [ "IndicatorLunar", "indicator-lunar", "1.0.80" ],
+    [ "IndicatorFortune", "indicator-fortune", "1.0.31" ],
+    [ "IndicatorLunar", "indicator-lunar", "1.0.81" ],
     [ "IndicatorOnThisDay", "indicator-on-this-day", "1.0.6" ],
-    [ "IndicatorPPADownloadStatistics", "indicator-ppa-download-statistics", "1.0.67" ],
-    [ "IndicatorPunycode", "indicator-punycode", "1.0.8" ],
+    [ "IndicatorPPADownloadStatistics", "indicator-ppa-download-statistics", "1.0.68" ],
+    [ "IndicatorPunycode", "indicator-punycode", "1.0.9" ],
     [ "IndicatorScriptRunner", "indicator-script-runner", "1.0.11" ],
     [ "IndicatorStardate", "indicator-stardate", "1.0.38" ],
-    [ "IndicatorTide", "indicator-tide", "1.0.19" ],
-    [ "IndicatorVirtualBox", "indicator-virtual-box", "1.0.62" ] ]
+    [ "IndicatorTide", "indicator-tide", "1.0.20" ],
+    [ "IndicatorVirtualBox", "indicator-virtual-box", "1.0.63" ] ]
 
 
 for project, source, versionOnPPA in indicators:
