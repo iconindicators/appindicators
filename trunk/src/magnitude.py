275f8e9a332f85e45c802965b9a4142c5eec5fb4
bernmeister <thebernmeister@hotmail.com>
2020-07-25 13:09:05 +0000

    More on Skyfield comets/minorplanets.

--- trunk/src/magnitude.py
+++ trunk/src/magnitude.py
@@ -27,7 +27,7 @@ def pyephemCometMinorPlanet( now, latitude, longitude, name, data, isComet ):
         apparentMagnitude = calculateApparentMagnitude_gk( body._g, body._k, body.earth_distance, body.sun_distance )
 
     else:
-        apparentMagnitude = calculateApparentMagnitude_GH( body._G, body._H, body.earth_distance, body.sun_distance, sun.earth_distance )
+        apparentMagnitude = calculateApparentMagnitude_HG( body._H, body._G, body.earth_distance, body.sun_distance, sun.earth_distance )
 
     print( "PyEphem", name,
            "\n\tAbs Mag:", body.mag,
@@ -42,17 +42,14 @@ def skyfieldCometMinorPlanet( now, latitude, longitude, name, data, isComet ):
     sun = ephemeris[ "sun" ]
     earth = ephemeris[ "earth" ]
 
-    if isComet:
-        with io.BytesIO( data.encode() ) as f:
+    with io.BytesIO( data.encode() ) as f:
+        if isComet:
             dataframe = skyfield.data.mpc.load_comets_dataframe( f ).set_index( "designation", drop = False )
+            body = sun + skyfield.data.mpc.comet_orbit( dataframe.loc[ name ], timeScale, skyfield.constants.GM_SUN_Pitjeva_2005_km3_s2 )
 
-        body = sun + skyfield.data.mpc.comet_orbit( dataframe.loc[ name ], timeScale, skyfield.constants.GM_SUN_Pitjeva_2005_km3_s2 )
-
-    else:
-        with io.BytesIO( data.encode() ) as f:
+        else:
             dataframe = skyfield.data.mpc.load_mpcorb_dataframe( f ).set_index( "designation", drop = False )
-
-        body = sun + skyfield.data.mpc.mpcorb_orbit( dataframe.loc[ name ], timeScale, skyfield.constants.GM_SUN_Pitjeva_2005_km3_s2 )
+            body = sun + skyfield.data.mpc.mpcorb_orbit( dataframe.loc[ name ], timeScale, skyfield.constants.GM_SUN_Pitjeva_2005_km3_s2 )
 
     t = timeScale.utc( now.year, now.month, now.day, now.hour, now.minute, now.second )
     alt, az, earthSunDistance = ( earth + topos ).at( t ).observe( sun ).apparent().altaz()
@@ -60,7 +57,7 @@ def skyfieldCometMinorPlanet( now, latitude, longitude, name, data, isComet ):
     alt, az, earthBodyDistance = ( earth + topos ).at( t ).observe( body ).apparent().altaz()
     ra, dec, earthBodyDistance = ( earth + topos ).at( t ).observe( body ).radec()
 
-    apparentMagnitude = calculateApparentMagnitude_GH( dataframe.loc[ name ][ "magnitude_G" ], dataframe.loc[ name ][ "magnitude_H" ], earthBodyDistance.au, sunBodyDistance.au, earthSunDistance.au )
+    apparentMagnitude = calculateApparentMagnitude_HG( dataframe.loc[ name ][ "magnitude_H" ], dataframe.loc[ name ][ "magnitude_G" ], earthBodyDistance.au, sunBodyDistance.au, earthSunDistance.au )
 
     print( "Skyfield", name,
            "\n\tAbs Mag:", dataframe.loc[ name ][ "magnitude_H" ],
@@ -73,7 +70,8 @@ def calculateApparentMagnitude_gk( g_absoluteMagnitude, k_luminosityIndex, bodyE
 
 
 # https://www.clearskyinstitute.com/xephem/help/xephem.html#mozTocId564354
-def calculateApparentMagnitude_GH( G_slope, H_absoluteMagnitude, bodyEarthDistance, bodySunDistance, earthSunDistance ):
+# https://www.britastro.org/asteroids/dymock4.pdf
+def calculateApparentMagnitude_HG( H_absoluteMagnitude, G_slope, bodyEarthDistance, bodySunDistance, earthSunDistance ):
     beta = math.acos( ( bodySunDistance * bodySunDistance + bodyEarthDistance * bodyEarthDistance - earthSunDistance * earthSunDistance ) / ( 2 * bodySunDistance * bodyEarthDistance ) )
     psi_t = math.exp( math.log10( math.tan( beta / 2.0 ) ) * 0.63 )
     Psi_1 = math.exp( -3.33 * psi_t )
@@ -89,16 +87,18 @@ longitude = 151
 cometName = "88P/Howell"
 
 # https://minorplanetcenter.net/iau/Ephemerides/Comets/Soft03Cmt.txt
-cometDataPyEphem = "88P/Howell,e,4.3838,56.6855,235.9159,3.105737,0.1800765,0.56433120,347.8225,07/21.0/2020,2000,g 11.0,6.0"
+cometDataPyEphem = "88P/Howell,e,4.3838,56.6855,235.9158,3.105749,0.1800755,0.56433269,348.3628,07/24.0/2020,2000,g 11.0,6.0"
 
+# Format: https://minorplanetcenter.net/iau/info/CometOrbitFormat.html
 # https://minorplanetcenter.net/iau/Ephemerides/Comets/Soft00Cmt.txt
-cometDataSkyfield = "0088P         2020 09 26.6241  1.353073  0.564331  235.9159   56.6855    4.3838  20200721  11.0  6.0  88P/Howell                                               MPEC 2019-JE2"
+cometDataSkyfield = "0088P         2020 09 26.6239  1.353073  0.564333  235.9158   56.6855    4.3838  20200724  11.0  6.0  88P/Howell                                               MPEC 2019-JE2"
 
 minorPlanetName = "(1) Ceres"
 
 # https://minorplanetcenter.net/iau/Ephemerides/Bright/2018/Soft03Bright.txt
 minorPlanetDataPyEphem = "1 Ceres,e,10.5935,80.3099,73.1153,2.767046,0.2141309,0.07553468,352.2305,03/23.0/2018,2000,H 3.34,0.12"
 
+# Format: https://minorplanetcenter.net/iau/info/MPOrbitFormat.html
 # https://minorplanetcenter.net/iau/Ephemerides/Bright/2018/Soft00Bright.txt
 minorPlanetDataSkyfield = "00001    3.34  0.12 K183N 352.23052   73.11528   80.30992   10.59351  0.0755347  0.21413094   2.7670463  0 MPO431490  6689 114 1801-2018 0.60 M-v 30h MPC        0000              (1) Ceres"
 
