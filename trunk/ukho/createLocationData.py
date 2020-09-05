f952ff3cd999492210b9265330928378702c95fb
bernmeister <thebernmeister@hotmail.com>
2020-09-05 07:16:19 +0000

    Tidy up.

--- trunk/ukho/createLocationData.py
+++ trunk/ukho/createLocationData.py
@@ -2,28 +2,37 @@
 # -*- coding: utf-8 -*-
 
 
-# Open the UKHO ports file
+# Run LibreOffice Calc, open the file
 #
 #     2018 Current Tidal Prediction Ports.xlsx
 #
-# in LibreOffice Calc and Save As 'Text CSV' using TAB as the field delimiter and no text delimiter.
+# then Save As 'Text CSV' with the filename
+#
+#     Current Tidal Prediction Ports.csv
+#
+# using TAB as the field delimiter.
 #
 # Note that openpyxl would not work as it gave some Unicode error, so must convert to CSV and read that instead.
 #
-# Amend the column numbers to match the port ID, port name and country and then call process to produce output of the form:
+# Ensure the columns in the CSV file match as follows:
+#     A = Port Number
+#     B = Suffix
+#     C = Port Name
+#     I = Country
+#
+# Then run the code below to produce an output of the form:
 #     [ "1822", "Alger (Algiers)", "Algeria" ],
 #     [ "1820", "Arzew", "Algeria" ],
+#     [ "2065B", "Bleaker Island", "Falkland Islands" ],
 #     ...
+#
+# which can then be used to paste into ports.py
 
 
-def process( fileName ):
-    with open( fileName ) as f:
-       for line in f:
-           if not line.startswith( "PortNo" ):
-               line = line.strip().split( "\t" )
-               line = "[ \"" + line[ 0 ] + line[ 1 ] + "\", \"" + line[ 2 ] + "\", \"" + line[ 8 ] + "\" ],"
-               line = line.title().replace( "St. ", "St." ).replace( "St.", "St. " ).replace( "'S", "'s" )
-               print( line )
-
-
-process( "Current Tidal Prediction Ports.csv" )
+with open( "Current Tidal Prediction Ports.csv" ) as f:
+   for line in f:
+       if not line.startswith( "PortNo" ):
+           line = line.strip().split( "\t" )
+           line = "[ \"" + line[ 0 ] + line[ 1 ] + "\", \"" + line[ 2 ] + "\", \"" + line[ 8 ] + "\" ],"
+           line = line.title().replace( "St. ", "St." ).replace( "St.", "St. " ).replace( "'S", "'s" )
+           print( line )
