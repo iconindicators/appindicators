446e1c0585434bb4d7e5e6b4f449a3fa0366753e
bernmeister <thebernmeister@hotmail.com>
2020-09-13 23:25:49 +0000

    In progres...

--- trunk/src/testEclipse.py
+++ trunk/src/testEclipse.py
@@ -2,26 +2,46 @@
 # -*- coding: utf-8 -*-
 
 
-import calendar, datetime, math
+import calendar, datetime, ephem, math
 
 
-def normaliseDegrees( degrees ): 
-    return ( degrees % 360 )
-#     return degrees
+def getMoonDatesForOneYearPyEphem( utcNow, isFull, latitude, longitude, elevation ):
+    city = ephem.city( "London" ) # Put in a city name known to exist in PyEphem then doctor to the correct lat/long/elev.
+    city.lat = str( latitude )
+    city.lon = str( longitude )
+    city.elev = elevation
+
+    moon = ephem.Moon()
+
+    dates = [ ]
+    date = utcNow
+    for i in range( 12 ):
+        city.date = ephem.Date( date )
+        moon.compute( city )
+        if isFull:
+            dates.append( ephem.next_full_moon( date ).datetime() )
+
+        else:
+            dates.append( ephem.next_new_moon( date ).datetime() )
+
+        date += datetime.timedelta( days = 20 )
+
+    return dates
 
 
-def getEclipseMeeus( utcNow, solar ):
+def normaliseDegrees( degrees ): return ( degrees % 360 )
+
+
+def getEclipseMeeus( utcNow, isSolar, upcomingMoonDates ):
     daysInYear = 365
     if calendar.isleap( utcNow.year ):
         daysInYear = 366
 
     yearAsDecimal = utcNow.year + ( utcNow.timetuple().tm_yday / daysInYear )
     k = ( yearAsDecimal - 2000 ) * 12.3685 # Equation 49.2
-    if solar:
-        k = round( k ) # Must be an integer for a New Moon (solar eclipse).
-
-    else:
-        k = None #TODO How to do this?
+    k = round( k ) # Must be an integer for a New Moon (solar eclipse).
+    if not isSolar:
+        k += 0.5 # Must be an integer increased by 0.5 for a Full Moon (lunar eclipse).
 
     T =  k /1236.85 # Equation 49.3
     JDE = 2451550.09766 + \
@@ -38,13 +58,13 @@ def getEclipseMeeus( utcNow, solar ):
 
     M = normaliseDegrees( M )
 
-    Mprime = 201.5634 + \
-             385.81693528 * k + \
-             0.0107582 * T * T + \
-             0.00001238 * T * T * T - \
-             0.000000058 * T * T * T * T # Equation 49.5
+    Mdash = 201.5634 + \
+            385.81693528 * k + \
+            0.0107582 * T * T + \
+            0.00001238 * T * T * T - \
+            0.000000058 * T * T * T * T # Equation 49.5
 
-    Mprime = normaliseDegrees( Mprime )
+    Mdash = normaliseDegrees( Mdash )
 
     F = 160.7108 + \
         390.67050284 * k - \
@@ -70,5 +90,14 @@ def getEclipseMeeus( utcNow, solar ):
     return
 
 
-getEclipseMeeus( datetime.datetime.strptime( "1993-05-21", "%Y-%m-%d" ), True )
+latitude = -33
+longitude = 151
+elevation = 0
+
+utcNow = datetime.datetime.strptime( "1997-07-01", "%Y-%m-%d" )
+fullMoonDates = getMoonDatesForOneYearPyEphem( utcNow, True, latitude, longitude, elevation)
+
+
+getEclipseMeeus( utcNow, False )
+# getEclipseMeeus( datetime.datetime.strptime( "1993-05-21", "%Y-%m-%d" ), True )
 # getEclipseMeeus( datetime.datetime.utcnow(), True )
