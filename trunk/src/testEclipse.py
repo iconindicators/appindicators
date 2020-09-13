2c4e559616dc2d5449d7c419bcfacf6131b9b212
bernmeister <thebernmeister@hotmail.com>
2020-09-13 13:09:05 +0000

    In progress.

--- trunk/src/testEclipse.py
+++ trunk/src/testEclipse.py
@@ -2,26 +2,33 @@
 # -*- coding: utf-8 -*-
 
 
-import calendar, datetime
+import calendar, datetime, math
 
 
-def normaliseDegrees( degrees ): return ( degrees % 360 )
+def normaliseDegrees( degrees ): 
+    return ( degrees % 360 )
+#     return degrees
 
 
-def getEclipseLunarMeeus( utcNow ):
+def getEclipseMeeus( utcNow, solar ):
     daysInYear = 365
     if calendar.isleap( utcNow.year ):
         daysInYear = 366
 
     yearAsDecimal = utcNow.year + ( utcNow.timetuple().tm_yday / daysInYear )
-    yearAsDecimal = 1977.13 #TODO Test
-    k = ( yearAsDecimal - 2000 ) * 12.3685 # Equation 49.2 
+    k = ( yearAsDecimal - 2000 ) * 12.3685 # Equation 49.2
+    if solar:
+        k = round( k ) # Must be an integer for a New Moon (solar eclipse).
+
+    else:
+        k = None #TODO How to do this?
+
     T =  k /1236.85 # Equation 49.3
-    julianEphemerisDays = 2451550.09766 + \
-                          29.530588861 * k + \
-                          0.00015437 * T * T - \
-                          0.000000150 * T * T * T + \
-                          0.00000000073 * T * T * T * T # Equation 49.1
+    JDE = 2451550.09766 + \
+          29.530588861 * k + \
+          0.00015437 * T * T - \
+          0.000000150 * T * T * T + \
+          0.00000000073 * T * T * T * T # Equation 49.1
 
     E = 1 - 0.002516 * T - 0.0000074 * T * T # Equation 47.6
     M = 2.5534 + \
@@ -47,14 +54,21 @@ def getEclipseLunarMeeus( utcNow ):
 
     F = normaliseDegrees( F )
 
-    Omega = 124.7746 - \
+    omega = 124.7746 - \
             1.56375588 * k + \
             0.0020672 * T * T + \
             0.00000215 * T * T * T # Equation 49.7
 
-    Omega = normaliseDegrees( Omega )
+    omega = normaliseDegrees( omega )
+
+    noEclipse = math.fabs( math.sin( math.radians( F ) ) ) > 0.36
+
+    F1 = F - 0.02665 * math.sin( math.radians( omega ) )
+
+    A1 = 299.77 + 0.107408 * k - 0.009173 * T * T
 
     return
 
 
-getEclipseLunarMeeus( datetime.datetime.utcnow() )
+getEclipseMeeus( datetime.datetime.strptime( "1993-05-21", "%Y-%m-%d" ), True )
+# getEclipseMeeus( datetime.datetime.utcnow(), True )
