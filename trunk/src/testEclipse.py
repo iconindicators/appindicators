5ddca13f7d3946dbf2d89f586808f2f8251cd02b
bernmeister <thebernmeister@hotmail.com>
2020-09-14 05:56:36 +0000

    In progress...

--- trunk/src/testEclipse.py
+++ trunk/src/testEclipse.py
@@ -5,7 +5,7 @@
 import calendar, datetime, ephem, math
 
 
-def getMoonDatesForOneYearPyEphem( utcNow, isFull, latitude, longitude, elevation ):
+def getMoonDatesForOneYearPyEphem( utcNow, isFullMoon, latitude, longitude, elevation ):
     city = ephem.city( "London" ) # Put in a city name known to exist in PyEphem then doctor to the correct lat/long/elev.
     city.lat = str( latitude )
     city.lon = str( longitude )
@@ -18,7 +18,7 @@ def getMoonDatesForOneYearPyEphem( utcNow, isFull, latitude, longitude, elevatio
     for i in range( 12 ):
         city.date = ephem.Date( date )
         moon.compute( city )
-        if isFull:
+        if isFullMoon:
             dates.append( ephem.next_full_moon( date ).datetime() )
 
         else:
@@ -27,9 +27,6 @@ def getMoonDatesForOneYearPyEphem( utcNow, isFull, latitude, longitude, elevatio
         date = dates[ -1 ] + datetime.timedelta( days = 1 ) # Look for next full/new moon one starting one day later.
 
     return dates
-
-
-def normaliseDegrees( degrees ): return ( degrees % 360 )
 
 
 def julianDayToGregorian( julianDay ):
@@ -60,16 +57,15 @@ def julianDayToGregorian( julianDay ):
     else:
         year = C - 4715
 
-    return
+    return datetime.datetime( year, month, 1 ) + datetime.timedelta( days = dayOfMonth - 1 )
 
 
 def getEclipseMeeus( utcNow, isSolar, upcomingMoonDates ):
-    foundEclipse = False
+    eclipseDate = None
     date = utcNow
     moonIndex = 0
-    while not foundEclipse: #TODO Better loop over the upcoming moon dates instead.
+    while moonIndex <= len( upcomingMoonDates ):
         daysInYear = 365
-#TODO Be careful here...if we are toward the end of the year and the next eclipse will be in the following year, may need to adjust the days.
         if calendar.isleap( date.year ):
             daysInYear = 366
 
@@ -89,41 +85,33 @@ def getEclipseMeeus( utcNow, isSolar, upcomingMoonDates ):
 
         E = 1 - 0.002516 * T - 0.0000074 * T * T # Equation 47.6
 
-        M =   2.5534 \
-            + 29.10536570 * k \
-            - 0.0000014 * T * T \
-            - 0.00000011 * T * T * T # Equation 49.4
+        M = (  2.5534 \
+             + 29.10536570 * k \
+             - 0.0000014 * T * T \
+             - 0.00000011 * T * T * T ) % 360 # Equation 49.4
 
-        M = normaliseDegrees( M )
+        Mdash = (  201.5634 \
+                 + 385.81693528 * k \
+                 + 0.0107582 * T * T \
+                 + 0.00001238 * T * T * T \
+                 - 0.000000058 * T * T * T * T ) % 360 # Equation 49.5
 
-        Mdash =   201.5634 \
-                + 385.81693528 * k \
-                + 0.0107582 * T * T \
-                + 0.00001238 * T * T * T \
-                - 0.000000058 * T * T * T * T # Equation 49.5
-
-        Mdash = normaliseDegrees( Mdash )
-
-        F =   160.7108 \
-            + 390.67050284 * k \
-            - 0.0016118 * T * T \
-            - 0.000000227 * T * T * T \
-            + 0.000000011 * T * T * T * T # Equation 49.6
-
-        F = normaliseDegrees( F )
+        F = (  160.7108 \
+             + 390.67050284 * k \
+             - 0.0016118 * T * T \
+             - 0.000000227 * T * T * T \
+             + 0.000000011 * T * T * T * T ) % 360 # Equation 49.6
 
         noEclipse = math.fabs( math.sin( math.radians( F ) ) ) > 0.36
         if noEclipse:
-            date = upcomingMoonDates[ moonIndex ]
+            date = upcomingMoonDates[ moonIndex ] #TODO Maybe add one day?
             moonIndex += 1
             continue
 
-        omega =   124.7746 \
-                - 1.56375588 * k \
-                + 0.0020672 * T * T \
-                + 0.00000215 * T * T * T # Equation 49.7
-
-        omega = normaliseDegrees( omega )
+        omega = (  124.7746 \
+                 - 1.56375588 * k \
+                 + 0.0020672 * T * T \
+                 + 0.00000215 * T * T * T ) % 360 # Equation 49.7
 
         F1 = F - 0.02665 * math.sin( math.radians( omega ) )
 
@@ -155,82 +143,48 @@ def getEclipseMeeus( utcNow, isSolar, upcomingMoonDates ):
                         - 0.0002 * E * math.sin( math.radians( 2 * Mdash - M ) ) \
                         - 0.0002 * math.sin( math.radians( omega ) )
 
-        x = julianDayToGregorian( correctedJDE )
+        eclipseDate = julianDayToGregorian( correctedJDE )
 
-        x = 1
-        x+=1
+        P =   0.2070 * E * math.sin( math.radians( M ) ) \
+            + 0.0024 * E * math.sin( math.radians( 2 * M ) ) \
+            - 0.0392 * math.sin( math.radians( Mdash ) ) \
+            + 0.0116 * math.sin( math.radians( 2 * Mdash ) ) \
+            - 0.0073 * E * math.sin( math.radians( Mdash + M ) ) \
+            + 0.0067 * E * math.sin( math.radians( Mdash - M ) ) \
+            + 0.0118 * math.sin( math.radians( 2 * F1 ) )
 
-    return
+        Q =   5.2207 \
+            - 0.0048 * E * math.cos( math.radians( M ) ) \
+            + 0.0020 * E * math.cos( math.radians( 2 * M ) ) \
+            - 0.3299 * math.cos( math.radians( Mdash ) ) \
+            - 0.0060 * E * math.cos( math.radians( Mdash + M ) ) \
+            + 0.0041 * E * math.cos( math.radians( Mdash - M ) )
 
+        W = math.fabs( math.cos( math.radians( F1 ) ) )
 
-def getEclipseMeeusORIGINAL( utcNow, isSolar, upcomingMoonDates ):
-    daysInYear = 365
-#TODO Be careful here...if we are toward the end of the year and the next eclipse will be in the following year, may need to adjust the days.
-    if calendar.isleap( utcNow.year ):
-        daysInYear = 366
+        gamma = ( P * math.cos( math.radians( F1 ) ) + Q * math.sin( math.radians( F1 ) ) ) * ( 1 - 0.0048 * W )
+        
+        u =   0.0059 \
+            + 0.0046 * E * math.cos( math.radians( M ) ) \
+            - 0.0182 * math.cos( math.radians( Mdash ) ) \
+            + 0.0004 * math.cos( math.radians( 2 * Mdash ) ) \
+            - 0.0005 * math.cos( math.radians( M + Mdash ) )
 
-    yearAsDecimal = utcNow.year + ( utcNow.timetuple().tm_yday / daysInYear )
-    k = ( yearAsDecimal - 2000 ) * 12.3685 # Equation 49.2
-    k = round( k ) # Must be an integer for a New Moon (solar eclipse).
-    if not isSolar:
-        k += 0.5 # Must be an integer increased by 0.5 for a Full Moon (lunar eclipse).
+        break
 
-    T =  k /1236.85 # Equation 49.3
-    JDE = 2451550.09766 + \
-          29.530588861 * k + \
-          0.00015437 * T * T - \
-          0.000000150 * T * T * T + \
-          0.00000000073 * T * T * T * T # Equation 49.1
-
-    E = 1 - 0.002516 * T - 0.0000074 * T * T # Equation 47.6
-    M = 2.5534 + \
-        29.10536570 * k - \
-        0.0000014 * T * T - \
-        0.00000011 * T * T * T # Equation 49.4
-
-    M = normaliseDegrees( M )
-
-    Mdash = 201.5634 + \
-            385.81693528 * k + \
-            0.0107582 * T * T + \
-            0.00001238 * T * T * T - \
-            0.000000058 * T * T * T * T # Equation 49.5
-
-    Mdash = normaliseDegrees( Mdash )
-
-    F = 160.7108 + \
-        390.67050284 * k - \
-        0.0016118 * T * T - \
-        0.000000227 * T * T * T + \
-        0.000000011 * T * T * T * T # Equation 49.6
-
-    F = normaliseDegrees( F )
-
-    noEclipse = math.fabs( math.sin( math.radians( F ) ) ) > 0.36
-
-    omega = 124.7746 - \
-            1.56375588 * k + \
-            0.0020672 * T * T + \
-            0.00000215 * T * T * T # Equation 49.7
-
-    omega = normaliseDegrees( omega )
-
-    F1 = F - 0.02665 * math.sin( math.radians( omega ) )
-
-    A1 = 299.77 + 0.107408 * k - 0.009173 * T * T
-
-    return
+    return eclipseDate
 
 
 latitude = -33
 longitude = 151
 elevation = 0
 
-utcNow = datetime.datetime.strptime( "1997-07-01", "%Y-%m-%d" )
+# utcNow = datetime.datetime.strptime( "1997-07-01", "%Y-%m-%d" )
+utcNow = datetime.datetime.utcnow()
 fullMoonDates = getMoonDatesForOneYearPyEphem( utcNow, True, latitude, longitude, elevation )
 newMoonDates = getMoonDatesForOneYearPyEphem( utcNow, False, latitude, longitude, elevation )
 
-
-getEclipseMeeus( utcNow, False, fullMoonDates )
+print( "Meeus solar eclipse:", getEclipseMeeus( utcNow, True, newMoonDates ) )
+print( "Meeus lunar eclipse:", getEclipseMeeus( utcNow, False, fullMoonDates ) )
 # getEclipseMeeus( datetime.datetime.strptime( "1993-05-21", "%Y-%m-%d" ), True )
 # getEclipseMeeus( datetime.datetime.utcnow(), True )
