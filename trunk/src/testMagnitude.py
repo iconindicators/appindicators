eceb08fb8841a1485c6e831bb67065c2d4b0200e
bernmeister <thebernmeister@hotmail.com>
2020-11-23 03:30:16 +0000

    Minor fix.

--- trunk/src/testMagnitude.py
+++ trunk/src/testMagnitude.py
@@ -81,7 +81,7 @@ def calculateApparentMagnitude_gk( g_absoluteMagnitude, k_luminosityIndex, bodyE
 # https://www.britastro.org/asteroids/dymock4.pdf
 def calculateApparentMagnitude_HG( H_absoluteMagnitude, G_slope, bodyEarthDistance, bodySunDistance, earthSunDistance ):
     beta = math.acos( ( bodySunDistance * bodySunDistance + bodyEarthDistance * bodyEarthDistance - earthSunDistance * earthSunDistance ) / ( 2 * bodySunDistance * bodyEarthDistance ) )
-    psi_t = math.exp( math.log10( math.tan( beta / 2.0 ) ) * 0.63 )
+    psi_t = math.exp( math.log( math.tan( beta / 2.0 ) ) * 0.63 )
     Psi_1 = math.exp( -3.33 * psi_t )
     psi_t = math.exp( math.log( math.tan( beta / 2.0 ) ) * 1.22 )
     Psi_2 = math.exp( -1.87 * psi_t )
