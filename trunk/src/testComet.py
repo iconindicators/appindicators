a328f0f92c7e1d8734b47d12957b36026222ff04
bernmeister <thebernmeister@hotmail.com>
2020-11-25 11:52:19 +0000

    Updated.

--- trunk/src/testComet.py
+++ trunk/src/testComet.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 
-import datetime, io, skyfield.api, skyfield.constants, skyfield.data.mpc
+# Download and save 
+#     https://www.minorplanetcenter.net/iau/Ephemerides/Comets/Soft00Cmt.txt
+#     https://www.minorplanetcenter.net/iau/Ephemerides/Comets/Soft03Cmt.txt
 
 
-def testSkyfield( orbitalElementData, utcNow, latitude, longitude, elevation, isComet ):
+import datetime, ephem, io, skyfield.api, skyfield.constants, skyfield.data.mpc
+
+
+def testSkyfield( orbitalElementData, utcNow, latitude, longitude, elevation ):
     timeScale = skyfield.api.load.timescale( builtin = True )
     t = timeScale.utc( utcNow.year, utcNow.month, utcNow.day, utcNow.hour, utcNow.minute, utcNow.second )
     ephemeris = skyfield.api.load( "de421.bsp" )
@@ -19,47 +24,59 @@ def testSkyfield( orbitalElementData, utcNow, latitude, longitude, elevation, is
 
         f.seek( 0 )
 
-        if isComet:
-            dataframe = skyfield.data.mpc.load_comets_dataframe( f ).set_index( "designation", drop = False )
+        dataframe = skyfield.data.mpc.load_comets_dataframe( f ).set_index( "designation", drop = False )
 
-        else:
-            dataframe = skyfield.data.mpc.load_mpcorb_dataframe( f ).set_index( "designation", drop = False )
-
-            # https://github.com/skyfielders/python-skyfield/issues/449#issuecomment-694159517
-            dataframe = dataframe[ ~dataframe.semimajor_axis_au.isnull() ]
-
-#TODO By setting the index above for designation it allows access to the name below in the loop.
-# Does this slow things down?  Is there another way to get the name?
-#Do we need the name?
-    if isComet:
         for name, row in dataframe.iterrows():
             try:
                 body = sun + skyfield.data.mpc.comet_orbit( dataframe.loc[ name ], timeScale, skyfield.constants.GM_SUN_Pitjeva_2005_km3_s2 )
                 ra, dec, earthBodyDistance = ( earth + topos ).at( t ).observe( body ).radec()
                 ra, dec, sunBodyDistance = sun.at( t ).observe( body ).radec()
+
             except Exception as e:
                 print( name )
                 print( e )
 
-    else:
-        pass#TODO
+
+def testPyEphem( orbitalElementData, utcNow, latitude, longitude, elevation ):
+    observer = ephem.Observer()
+    observer.date = ephem.Date( utcNow )
+    observer.lat = str( latitude )
+    observer.lon = str( longitude )
+    observer.elev = elevation
+
+    for line in orbitalElementData:
+        if not line.startswith( "#" ): 
+            comet = ephem.readdb( line )
+            comet.compute( observer )
+            sun = ephem.Sun()
+            sun.compute( observer )
 
 
-# Download and save  
-# https://minorplanetcenter.net/iau/Ephemerides/Distant/Soft00Distant.txt   FIX THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# and adjust the date to the download date.
 utcNow = datetime.datetime.strptime( "2020-11-24", "%Y-%m-%d" )
 
 latitude = -33
 longitude = 151
 elevation = 100
 
-print( "Skyfield:", skyfield.__version__ )
-
 t = datetime.datetime.utcnow()
 
 with open( "Soft00Cmt.txt" ) as f:
     orbitalElementData = f.readlines()
 
-testSkyfield( orbitalElementData, utcNow, latitude, longitude, elevation, True )
-print( "Skyfield comet:", ( datetime.datetime.utcnow() - t ) )
+testSkyfield( orbitalElementData, utcNow, latitude, longitude, elevation )
+print( "Skyfield:", skyfield.__version__, '\n', "Duration:", datetime.datetime.utcnow() - t, '\n' )
+
+t = datetime.datetime.utcnow()
+
+with open( "Soft03Cmt.txt" ) as f:
+    orbitalElementData = f.readlines()
+
+testPyEphem( orbitalElementData, utcNow, latitude, longitude, elevation )
+print( "PyEphem:", ephem.__version__, '\n', "Duration:", datetime.datetime.utcnow() - t, '\n' )
+
+# Skyfield: 1.33 
+#  Duration: 0:02:03.965268 
+# 
+# PyEphem: 3.7.6.0 
+#  Duration: 0:00:00.002429 
+
--- trunk/src/testMinorPlanet.py
+++ trunk/src/testMinorPlanet.py
@@ -1,60 +1,87 @@
 #!/usr/bin/env python3
 
-import datetime, io, skyfield.api, skyfield.constants, skyfield.data.mpc
 
-print( "Skyfield:", skyfield.__version__ )
+# Download and save 
+#     https://minorplanetcenter.net/iau/Ephemerides/Bright/2018/Soft00Bright.txt
+#     https://minorplanetcenter.net/iau/Ephemerides/Bright/2018/Soft03Bright.txt
 
-# Download and save  
-# https://minorplanetcenter.net/iau/Ephemerides/Distant/Soft00Distant.txt
-# and adjust the date to the download date.
-now = datetime.datetime.strptime( "2020-09-16", "%Y-%m-%d" )
+
+import datetime, ephem, io, skyfield.api, skyfield.constants, skyfield.data.mpc
+
+
+def testSkyfield( orbitalElementData, utcNow, latitude, longitude, elevation ):
+    timeScale = skyfield.api.load.timescale( builtin = True )
+    t = timeScale.utc( utcNow.year, utcNow.month, utcNow.day, utcNow.hour, utcNow.minute, utcNow.second )
+    ephemeris = skyfield.api.load( "de421.bsp" )
+    sun = ephemeris[ "sun" ]
+    earth = ephemeris[ "earth" ]
+    topos = skyfield.api.Topos( latitude_degrees = latitude, longitude_degrees = longitude, elevation_m = elevation )
+    alt, az, earthSunDistance = ( earth + topos ).at( t ).observe( sun ).apparent().altaz()
+
+    with io.BytesIO() as f:
+        for orbitalElement in orbitalElementData:
+            f.write( ( orbitalElement + '\n' ).encode() )
+
+        f.seek( 0 )
+
+        dataframe = skyfield.data.mpc.load_mpcorb_dataframe( f ).set_index( "designation", drop = False )
+
+        # Remove potential bad data as per
+        # https://github.com/skyfielders/python-skyfield/issues/449#issuecomment-694159517
+        dataframe = dataframe[ ~dataframe.semimajor_axis_au.isnull() ]
+
+        for name, row in dataframe.iterrows():
+            try:
+                body = sun + skyfield.data.mpc.mpcorb_orbit( dataframe.loc[ name ], timeScale, skyfield.constants.GM_SUN_Pitjeva_2005_km3_s2 )
+                ra, dec, earthBodyDistance = ( earth + topos ).at( t ).observe( body ).radec()
+                ra, dec, sunBodyDistance = sun.at( t ).observe( body ).radec()
+
+            except Exception as e:
+                print( name )
+                print( e )
+
+
+def testPyEphem( orbitalElementData, utcNow, latitude, longitude, elevation ):
+    observer = ephem.Observer()
+    observer.date = ephem.Date( utcNow )
+    observer.lat = str( latitude )
+    observer.lon = str( longitude )
+    observer.elev = elevation
+
+    for line in orbitalElementData:
+        if not line.startswith( "#" ): 
+            comet = ephem.readdb( line )
+            comet.compute( observer )
+            sun = ephem.Sun()
+            sun.compute( observer )
+
+
+utcNow = datetime.datetime.strptime( "2020-11-24", "%Y-%m-%d" )
 
 latitude = -33
 longitude = 151
-timeScale = skyfield.api.load.timescale( builtin = True )
-t = timeScale.utc( now.year, now.month, now.day, now.hour, now.minute, now.second )
-print( t.utc_jpl() )
-topos = skyfield.api.Topos( latitude_degrees = latitude, longitude_degrees = longitude )
-ephemeris = skyfield.api.load( "de421.bsp" )
-sun = ephemeris[ "sun" ]
-earth = ephemeris[ "earth" ]
-# source = "Soft00Unusual.txt"
-source = "Soft00Distant.txt"
-# with open( source ) as f:
-#    for line in f:
-#        with io.BytesIO( line.encode() ) as f:
-#            dataframe = skyfield.data.mpc.load_mpcorb_dataframe( f ).set_index( "designation", drop = False )
-#            name = line[ 166 : 194 ].strip()
-#            body = sun + skyfield.data.mpc.mpcorb_orbit( dataframe.loc[ name ], timeScale, skyfield.constants.GM_SUN_Pitjeva_2005_km3_s2 )
-#            try:
-#                ra, dec, sunBodyDistance = sun.at( t ).observe( body ).radec()
-#                ra, dec, earthBodyDistance = ( earth + topos ).at( t ).observe( body ).radec()
-#            except Exception as e:
-#                print( name, e )
+elevation = 100
+
+t = datetime.datetime.utcnow()
+
+with open( "Soft00Bright.txt" ) as f:
+    orbitalElementData = f.readlines()
+
+testSkyfield( orbitalElementData, utcNow, latitude, longitude, elevation )
+print( "Skyfield:", skyfield.__version__, '\n', "Duration:", datetime.datetime.utcnow() - t, '\n' )
+
+t = datetime.datetime.utcnow()
+
+with open( "Soft03Bright.txt" ) as f:
+    orbitalElementData = f.readlines()
+
+testPyEphem( orbitalElementData, utcNow, latitude, longitude, elevation )
+print( "PyEphem:", ephem.__version__, '\n', "Duration:", datetime.datetime.utcnow() - t, '\n' )
 
 
-#TODO Hit an exception and logged it
-# https://github.com/skyfielders/python-skyfield/issues/449
-# So need to reproduce (maybe in test code)
-# but also see if the suggestion of using the dataframe loaded once makes a difference (may need a new issue logged for that).
-data = "K17M07B 14.2   0.15 K1794   0.00140   80.46498   58.25502   55.71379  0.9987471  0.00000466                MPO435133    33   1  174 days 0.34         MPC        0000         2017 MB7"
-with io.BytesIO( data.encode() ) as f:
-    dataframe = skyfield.data.mpc.load_mpcorb_dataframe( f ).set_index( "designation", drop = False )
-    body = sun + skyfield.data.mpc.mpcorb_orbit( dataframe.loc[ "2017 MB7" ], timeScale, skyfield.constants.GM_SUN_Pitjeva_2005_km3_s2 )
-    try:
-        ra, dec, sunBodyDistance = sun.at( t ).observe( body ).radec()
-        ra, dec, earthBodyDistance = ( earth + topos ).at( t ).observe( body ).radec()
-    except Exception as e:
-        print( e )
+# Skyfield: 1.33 
+#  Duration: 0:00:07.129343 
+# 
+# PyEphem: 3.7.6.0 
+#  Duration: 0:00:00.000375 
 
-               
-# /usr/local/lib/python3.6/dist-packages/jplephem/spk.py:227: RuntimeWarning: invalid value encountered in double_scalars
-#   index2, offset2 = divmod(tdb2 * S_PER_DAY, intlen)
-# 2012 DR30 ephemeris segment only covers dates 1899-07-28 23:59:18Z through 2053-10-08 23:58:51Z UT
-# /usr/local/lib/python3.6/dist-packages/jplephem/spk.py:228: RuntimeWarning: invalid value encountered in divmod
-#   index3, offset = divmod(offset1 + offset2, intlen)
-# 2014 FE72 ephemeris segment only covers dates 1899-07-28 23:59:18Z through 2053-10-08 23:58:51Z UT
-# 2015 TG387 ephemeris segment only covers dates 1899-07-28 23:59:18Z through 2053-10-08 23:58:51Z UT
-# 2016 FL59 ephemeris segment only covers dates 1899-07-28 23:59:18Z through 2053-10-08 23:58:51Z UT
-# 2017 MB7 ephemeris segment only covers dates 1899-07-28 23:59:18Z through 2053-10-08 23:58:51Z UT
-#                
