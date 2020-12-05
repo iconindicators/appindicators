ac7e7a244a1c2a44ae18b86779e6f33d5da391c2
bernmeister <thebernmeister@hotmail.com>
2020-12-05 01:37:45 +0000

    Updated.

--- trunk/src/checkUnreleased.py
+++ trunk/src/checkUnreleased.py
@@ -11,15 +11,15 @@ basePath = "../../"
 changeLogPath = "/packaging/debian/changelog"
 sourcePath = "/src/"
 indicators = [
-    [ "IndicatorFortune", "indicator-fortune", "1.0.33" ],
-    [ "IndicatorLunar", "indicator-lunar", "1.0.84" ],
-    [ "IndicatorOnThisDay", "indicator-on-this-day", "1.0.8" ],
-    [ "IndicatorPPADownloadStatistics", "indicator-ppa-download-statistics", "1.0.71" ],
-    [ "IndicatorPunycode", "indicator-punycode", "1.0.10" ],
-    [ "IndicatorScriptRunner", "indicator-script-runner", "1.0.12" ],
-    [ "IndicatorStardate", "indicator-stardate", "1.0.39" ],
-    [ "IndicatorTide", "indicator-tide", "1.0.22" ],
-    [ "IndicatorVirtualBox", "indicator-virtual-box", "1.0.65" ] ]
+    [ "IndicatorFortune", "indicator-fortune", "1.0.35" ],
+    [ "IndicatorLunar", "indicator-lunar", "1.0.85" ],
+    [ "IndicatorOnThisDay", "indicator-on-this-day", "1.0.9" ],
+    [ "IndicatorPPADownloadStatistics", "indicator-ppa-download-statistics", "1.0.73" ],
+    [ "IndicatorPunycode", "indicator-punycode", "1.0.11" ],
+    [ "IndicatorScriptRunner", "indicator-script-runner", "1.0.13" ],
+    [ "IndicatorStardate", "indicator-stardate", "1.0.40" ],
+    [ "IndicatorTide", "indicator-tide", "1.0.23" ],
+    [ "IndicatorVirtualBox", "indicator-virtual-box", "1.0.66" ] ]
 
 
 for project, source, versionOnPPA in indicators:
