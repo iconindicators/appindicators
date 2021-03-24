aaf92f9bf167fd383741e47a863d798990211e53
bernmeister <thebernmeister@hotmail.com>
2021-03-24 05:38:11 +0000

    Updated.

--- trunk/src/testOrbitalElement2.py
+++ trunk/src/testOrbitalElement2.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
 
-# Data sources:
-#     https://minorplanetcenter.net/iau/Ephemerides/Distant/Soft00Distant.txt 
-#     https://minorplanetcenter.net/iau/Ephemerides/Distant/Soft03Distant.txt
-#     https://minorplanetcenter.net/iau/Ephemerides/Unusual/Soft00Unusual.txt
-#     https://minorplanetcenter.net/iau/Ephemerides/Unusual/Soft03Unusual.txt
-
-
 from skyfield.data import mpc
 
 import datetime, ephem, io
@@ -41,14 +34,20 @@ def testPyEphem( orbitalElementData, utcNow, latitude, longitude, elevation ):
         observer.elev = elevation
         orbitalElement = ephem.readdb( orbitalElementData )
         orbitalElement.compute( observer )
-        return orbitalElement._H, orbitalElement._G
 
+        if isinstance( orbitalElement, ephem.EllipticalBody ):
+            lastComma = orbitalElementData.rindex( "," )
+            secondLastComma = orbitalElementData[ : lastComma ].rindex( "," )
+            field12 = orbitalElementData[ secondLastComma + 1 : lastComma ]
+            if field12.startswith( 'g' ):
+                return orbitalElement._g, orbitalElement._k
 
-pyephemComet = "2010 LG61,e,123.8859,317.3744,352.1688,7.366687,0.0492942,0.81371070,163.4277,04/27.0/2019,2000,H,0.15"
-skyfieldComet = "K10L61G             K194R 163.42773  352.16876  317.37443  123.88594  0.8137107  0.04929419   7.3666867  7 MPO460897    17   1   55 days 0.84 M-v 38h MPC        0000         2010 LG61"
+            else:
+                return orbitalElement._H, orbitalElement._G
 
-pyephemMinorPlanet = "2010 AZ85,e,17.7666,63.7137,265.6580,1.447667,0.5658494,0.00008261,177.0566,01/04.0/2010,2000,H,0.15"
-skyfieldMinorPlanet = "K10A85Z             K1014 177.05658  265.65797   63.71372   17.76664  0.0000826  0.56584942   1.4476674  E MPO221139     9   1    1 days 0.47         MPC        0000         2010 AZ85"
+        else: # Assume HyperbolicBody ParabolicBody
+            return orbitalElement._g, orbitalElement._k
+
 
 utcNow = datetime.datetime.strptime( "2021-03-24", "%Y-%m-%d" ) # Set to the date matching the data files.
 
@@ -56,16 +55,44 @@ latitude = -33
 longitude = 151
 elevation = 100
 
-# Comet
-_H, _G = testPyEphem( pyephemComet, utcNow, latitude, longitude, elevation )
-print( _H, _G )
 
-_g, _k = testSkyfield( skyfieldComet, True )
+# Test PyEphem comets and minor planets...
+#
+# Data format:
+#     http://www.clearskyinstitute.com/xephem/help/xephem.html#mozTocId215848
+#
+# Comets data source:
+#     https://www.minorplanetcenter.net/iau/Ephemerides/Comets/Soft03Cmt.txt
+#
+# Minor Planets data source:
+#     https://minorplanetcenter.net/iau/Ephemerides/Distant/Soft03Distant.txt
+#
+# Field 2 = e => field 12 = g or H, field 13 = k or G, specified by a leading g or H in field 12 (no g or H implies H). 
+pyephemComet = "413P/Larson,e,15.9772,39.0258,186.0334,3.711010,0.1378687,0.42336952,343.5677,03/23.0/2021,2000,g 14.0,4.0" 
+_Hg, _Gk = testPyEphem( pyephemComet, utcNow, latitude, longitude, elevation )
+print( _Hg, _Gk )
+
+# Field 2 = h => field 10 = g, field 11 = k
+pyephemComet = "C/2015 D3 (PANSTARRS),h,05/02.3218/2016,128.5493,157.0632,2.9458,1.003528,8.142955,2000,5.5,4.0"
+_g, _k = testPyEphem( pyephemComet, utcNow, latitude, longitude, elevation )
 print( _g, _k )
 
-# Minor Planet
-_H, _G = testPyEphem( pyephemMinorPlanet, utcNow, latitude, longitude, elevation )
-print( _H, _G )
+# Field 2 = p => field 9 = g, field 10 = k
+pyephemComet = "C/2018 F3 (Johnson),p,08/15.2314/2017,105.5348,293.0113,2.483172,173.0311,2000,13.0,4.0"
+_g, _k = testPyEphem( pyephemComet, utcNow, latitude, longitude, elevation )
+print( _g, _k )
 
+# Field 2 = e => field 12 = g or H, field 13 = k or G, specified by a leading g or H in field 12 (no g or H implies H). 
+pyephemMinorPlanet = "2010 LG61,e,123.8859,317.3744,352.1688,7.366687,0.0492942,0.81371070,163.4277,04/27.0/2019,2000,H,0.15"
+_Hg, _Gk = testPyEphem( pyephemMinorPlanet, utcNow, latitude, longitude, elevation )
+print( _Hg, _Gk )  # INCORRECT: There is no value defined in the data for H.
+
+
+# Test Skyfield minor planets...
+#
+# Minor Planets data source:
+#     https://minorplanetcenter.net/iau/Ephemerides/Distant/Soft00Distant.txt
+#
+skyfieldMinorPlanet = "K10L61G             K194R 163.42773  352.16876  317.37443  123.88594  0.8137107  0.04929419   7.3666867  7 MPO460897    17   1   55 days 0.84 M-v 38h MPC        0000         2010 LG61"
 _H, _G = testSkyfield( skyfieldMinorPlanet, False )
 print( _H, _G )
