0ea7e6c92e983a7ea0fe3e83cbfec1c61c38a62e
bernmeister <thebernmeister@hotmail.com>
2021-09-09 03:21:59 +0000

    Checked off TODO.

--- trunk/src/indicator-ppa-download-statistics.py
+++ trunk/src/indicator-ppa-download-statistics.py
@@ -19,9 +19,6 @@
 # Application indicator which displays PPA download statistics.
 
 
-#TODO Do a search for      \[ \d \]    and for those indices, use a definition.
-
-
 INDICATOR_NAME = "indicator-ppa-download-statistics"
 import gettext
 gettext.install( INDICATOR_NAME )
