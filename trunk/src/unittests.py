71bc3f583282285c1fa641e06b467bd21f63ce29
bernmeister <thebernmeister@hotmail.com>
2021-09-13 02:18:42 +0000

    Added filters.

--- trunk/src/unittests.py
+++ trunk/src/unittests.py
@@ -39,30 +39,25 @@ class Test( unittest.TestCase ):
         myFilters.addFilter( user, name, series, architecture, filterText )
         self.assertAlmostEqual( myFilters.getFilterText( user, name, series, architecture ), filterText )
 
-        # for u, n, s, a in myFilters.getUserNameSeriesArchitecture():
-        #     self.assertAlmostEqual( myFilters.getFilterText( user, name, series, architecture ), filterText )
-        #     print( a, b, c, d )
+        for u, n, s, a in myFilters.getUserNameSeriesArchitecture():
+            self.assertAlmostEqual( u, user )
+            self.assertAlmostEqual( n, name )
+            self.assertAlmostEqual( s, series )
+            self.assertAlmostEqual( a, architecture )
 
-        # self.assertAlmostEqual( myPublishedBinary.getPackageName(), packageName )
-        # self.assertAlmostEqual( myPublishedBinary.getPackageVersion(), packageVersion )
-        # self.assertAlmostEqual( myPublishedBinary.getDownloadCount(), downloadCount )
-        # self.assertAlmostEqual( myPublishedBinary.isArchitectureSpecific(), architectureSpecific )
-        #
-        # self.assertAlmostEqual( myPublishedBinary.__eq__( PublishedBinary( packageName, packageVersion, downloadCount, architectureSpecific ) ), True )
-        # self.assertAlmostEqual( myPublishedBinary.__eq__( PublishedBinary( packageName + " ", packageVersion, downloadCount, architectureSpecific ) ), False )
-        # self.assertAlmostEqual( myPublishedBinary.__eq__( PublishedBinary( packageName, packageVersion + " ", downloadCount, architectureSpecific ) ), False )
-        # self.assertAlmostEqual( myPublishedBinary.__eq__( PublishedBinary( packageName, packageVersion, downloadCount + " ", architectureSpecific ) ), False )
-        # self.assertAlmostEqual( myPublishedBinary.__eq__( PublishedBinary( packageName, packageVersion, downloadCount, "true" ) ), False )
-        #
-        # publishedBinaryString = \
-        #     packageName + " | " + \
-        #     packageVersion + " | " + \
-        #     downloadCount + " | " + \
-        #     architectureSpecific
-        #
-        # self.assertAlmostEqual( myPublishedBinary.__str__(), publishedBinaryString )
-        #
-        # self.assertAlmostEqual( myPublishedBinary.__repr__(), publishedBinaryString )
+        filtersString = \
+            "{'filters': {'" + \
+             user + " | " + \
+             name + " | " + \
+             series + " | " + \
+             architecture + \
+             "': '" + \
+             filterText + \
+             "'}}"
+
+        self.assertAlmostEqual( myFilters.__str__(), filtersString )
+        
+        self.assertAlmostEqual( myFilters.__repr__(), filtersString )
 
 
     def testPublisedBinary( self ):
