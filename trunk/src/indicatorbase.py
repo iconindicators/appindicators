1fee1c72dc2dbd8e54c752933053494fb9ec6ead
bernmeister <thebernmeister@hotmail.com>
2021-10-27 13:09:42 +0000

    Now using subprocess.run

--- trunk/src/indicatorbase.py
+++ trunk/src/indicatorbase.py
@@ -822,8 +822,6 @@ class IndicatorBase( ABC ):
             result = subprocess.run( command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE ).stdout.decode()
             if not result:
                 result = None
-            
-            # result = subprocess.check_output( command, shell = True, stderr = subprocess.PIPE, universal_newlines = True )
 
         except subprocess.CalledProcessError as e:
             self.getLogging().error( e )
