b790378c0602700a3855405b0908e0e9b2b71696
bernmeister <thebernmeister@hotmail.com>
2022-06-25 01:30:16 +0000

    Updated TODO. Replaced deprecated API call.

--- indicator-script-runner/src/indicator-script-runner.py
+++ indicator-script-runner/src/indicator-script-runner.py
@@ -35,18 +35,6 @@ from threading import Thread
 import concurrent.futures, copy, datetime, math
 
 
-#TODO
-# Edit the properties for "Internet Running Monthly Quota" to leave terminal open.  Run the script (click on it from icon menu)....no result and no terminal...why?
-#
-# This came about when I created a new "script" with command:
-#
-#     free | awk '/Mem/{printf("RAM Usage: %.2f%\n"), $3/$2*100}'
-#
-# and wanted the terminal open to see the result...but no terminal and no result.
-#
-# ...and no log file!
-
-
 class IndicatorScriptRunner( IndicatorBase ):
 
     CONFIG_HIDE_GROUPS = "hideGroups"
@@ -141,7 +129,7 @@ class IndicatorScriptRunner( IndicatorBase ):
                 scriptsByGroup = self.getScriptsByGroup( self.scripts, True, False )
                 indent = self.getMenuIndent( 1 )
                 for group in sorted( scriptsByGroup.keys(), key = str.lower ):
-                    menu.append( Gtk.MenuItem( group + "..." ) )
+                    menu.append( Gtk.MenuItem.new_with_label( group + "..." ) )
                     self.addScriptsToMenu( scriptsByGroup[ group ], group, menu, indent )
 
 
@@ -170,6 +158,7 @@ class IndicatorScriptRunner( IndicatorBase ):
             command += "; ${SHELL}"
 
         command += "'"
+        self.getLogging().debug( script.getGroup() + " | " + script.getName() + ": " + command ) #TODO Need a debug option/preference...either at the script level, or global.
         Thread( target = self.processCall, args = ( command, ) ).start()
 
 
