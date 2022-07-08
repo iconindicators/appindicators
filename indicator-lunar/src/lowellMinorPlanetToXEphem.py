27c5943a6a06e9eba5c585df8fdfd8b69d2d6c61
bernmeister <thebernmeister@hotmail.com>
2022-07-08 00:36:17 +0000

    Initial commit.

---
+++ indicator-lunar/src/lowellMinorPlanetToXEphem.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+# Convert minor planet data in Lowell format to XEphem format.
+#
+# Inspired by:
+#    https://github.com/XEphem/XEphem/blob/main/GUI/xephem/auxil/astorb2edb.pl
+#
+# Lowell format: 
+#    https://asteroid.lowell.edu/main/astorb/
+#
+# XEphem format:
+#    https://xephem.github.io/XEphem/Site/help/xephem.html#mozTocId468501
+
+
+import gzip, sys
+
+
+def processAndWriteOneLine( line, outputFile ):
+    if len( line.strip() ) > 0:
+        name = line[ 1 - 1 : 26 ].replace( '(', '' ).replace( ')', '' ).strip()
+        absoluteMagnitude = line[ 43 - 1 : 49 ].strip() # $H
+
+        if len( name ) == 0:
+            print( "Missing name:\n" + line )
+
+        elif len( absoluteMagnitude ) == 0:
+            print( "Missing absolute magnitude:\n" + line )
+
+        else:
+            inclinationToEcliptic = line[ 149 - 1 : 158 ].strip() # $i
+            longitudeAscendingNode = line[ 138 - 1 : 148 ].strip() # $O
+            argumentPerihelion = line[ 127 - 1 : 137 ].strip() # $o
+            semimajorAxix = line[ 170 - 1 : 181 ].strip() # $a
+            orbitalEccentricity = line[ 159 - 1 : 169 ].strip() # $e
+            meanAnomalyEpoch = line[ 116 - 1 : 126 ].strip() # $M
+            epochDate = line[ 111 - 1 : 112 ] + '/' + line[ 113 - 1 : 114 ] + '/' + line[ 107 - 1 : 110 ]
+            slopeParamenter = line[ 50 - 1 : 54 ].strip() # $G
+
+            components = [
+                name, 'e', inclinationToEcliptic, longitudeAscendingNode,
+                argumentPerihelion, semimajorAxix, '0', orbitalEccentricity, meanAnomalyEpoch,
+                epochDate, "2000.0",
+                absoluteMagnitude, slopeParamenter ]
+
+            outputFile.write( ','.join( components ) + '\n' )
+
+
+def convert( inFile ):
+    outFile = inFile.replace( "dat", "edb" )
+    if inFile.endswith( ".gz" ):
+        fIn = gzip.open( inFile, 'r' )
+        outFile = outFile[ 0 : -3 ]
+
+    else:
+        fIn = open( inFile, 'r' )
+
+    fOut = open( outFile, 'w' )
+    for line in fIn:
+        processAndWriteOneLine( line, fOut )
+
+    fIn.close()
+    fOut.close()
+
+
+if len( sys.argv ) != 2:
+    message = \
+        "Usage: python3 " + sys.argv[ 0 ] + " fileToConvert" + \
+        "\n\nFor example:" + \
+        "\n  python3  " + sys.argv[ 0 ] + " astorb.dat" + \
+        "\n  python3  " + sys.argv[ 0 ] + " astorb.dat.gz"
+
+    raise SystemExit( message )
+
+convert( sys.argv[ 1 ] )
