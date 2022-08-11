d65ac07b42e15d8a147e173580050fad31e94b6e
bernmeister <thebernmeister@hotmail.com>
2022-08-11 11:43:19 +0000

    Updated TODO and turned off comets for Skyfield.

--- indicator-lunar/src/astroskyfield.py
+++ indicator-lunar/src/astroskyfield.py
@@ -878,7 +878,7 @@ class AstroSkyfield( AstroBase ):
             apparentMagnitudeMaximum,
             logging ):
 
-        if True: return #TODO Breaks when running Skyfield with comets.
+        if bodyType == AstroBase.BodyType.COMET: return #TODO Comets data file currently breaks this code...waiting on Jure from COBS to fix.
 
         # Skyfield loads orbital element data into a dataframe from a file; 
         # as the orbital element data is already in memory,
