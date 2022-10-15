b64344a2d785b2d53d72e4eb04507190022d884f
bernmeister <thebernmeister@hotmail.com>
2022-10-15 03:04:48 +0000

    Added TODO.

--- indicator-lunar/src/eclipse.py
+++ indicator-lunar/src/eclipse.py
@@ -16,6 +16,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+#TODO Switch to new code in testEclipseDate.py
+
+
 #TODO For Skfield, need to somehow translate eclipse type (maybe in astroskyfield) to the NEW eclipse type.
  
 
