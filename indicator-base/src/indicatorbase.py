94c0cb97e1ee6666795d97e9b8a933b7fe87d044
bernmeister <thebernmeister@hotmail.com>
2023-11-07 23:03:31 +0000

    Added TODO.

--- indicator-base/src/indicatorbase.py
+++ indicator-base/src/indicatorbase.py
@@ -228,6 +228,40 @@
 #   https://stackoverflow.com/questions/72352801/migration-from-setup-py-to-pyproject-toml-how-to-specify-package-name
 
 
+#TODO When installing indicator-lunar on Debian bookworm,
+# got an error similar to this:
+#
+#     error: externally-managed-environment
+#
+#     × This environment is externally managed
+#     ╰─> To install Python packages system-wide, try apt install
+#     python3-xyz, where xyz is the package you are trying to
+#     install.
+#
+#     If you wish to install a non-Debian-packaged Python package,
+#     create a virtual environment using python3 -m venv path/to/venv.
+#     Then use path/to/venv/bin/python and path/to/venv/bin/pip. Make
+#     sure you have python3-full installed.
+#
+#     If you wish to install a non-Debian packaged Python application,
+#     it may be easiest to use pipx install xyz, which will manage a
+#     virtual environment for you. Make sure you have pipx installed.
+#
+#     See /usr/share/doc/python3.11/README.venv for more information.
+#
+#     note: If you believe this is a mistake, please contact your Python installation or OS distribution provider. You can override this, at the risk of breaking your Python installation or OS, by passing --break-system-packages.
+#     hint: See PEP 668 for the detailed specification.
+#
+# I think this is because pip now needs to be run in a venv.
+# If that is the case will impact (in some way which I do not know)
+# the installation of ephem and sgp4 (and eventually skyfield).
+# What to do...?
+#
+# This was useful in exaplaing the problem but only good for solving
+# by using a venv.
+# https://stackoverflow.com/a/75696359/2156453
+
+
 import gi
 
 gi.require_version( "GLib", "2.0" )
