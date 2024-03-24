b66106e24bc1c1983c48e09d93e5cba4dac76cb9
bernmeister <thebernmeister@hotmail.com>
2024-03-24 09:43:04 +0000

    Removed spurious whitespace.

--- indicatorbase/src/indicatorbase/platform/linux/indicatorbase.sh
+++ indicatorbase/src/indicatorbase/platform/linux/indicatorbase.sh
@@ -2,5 +2,5 @@
 
 cd $HOME && \
 . .local/venv_{indicator_name}/bin/activate && \
-python3 $(ls -d $HOME/.local/venv_{indicator_name}/lib/python3.* | head -1)/site-packages/{indicator_name}/ {indicator_name}.py && \
+python3 $(ls -d $HOME/.local/venv_{indicator_name}/lib/python3.* | head -1)/site-packages/{indicator_name}/{indicator_name}.py && \
 deactivate
