25d83dc5ac9416222b852be3f14766df6559a336
bernmeister <thebernmeister@hotmail.com>
2024-03-25 12:24:53 +0000

    Added section on how to run the indicator from the terminal.

--- tools/build_readme.py
+++ tools/build_readme.py
@@ -437,7 +437,14 @@ def _get_usage( indicator_name ):
         f"To run `{ indicator_name }`, press the `Super`/`Windows` key to open the `Show Applications` overlay (or similar), "
         f"type `{ indicator_names[ indicator_name ].split( ' ', 1 )[ 1 ].lower() }` "
         f"into the search bar and the icon should be present for you to click.  "
-        f"If the icon does not appear, or appears as generic, you may have to log out and log back in (or restart).\n\n" )
+        f"If the icon does not appear, or appears as generic, you may have to log out and log back in (or restart).\n"
+        f"Alternatively, to run from the terminal:\n\n"
+        f"```\n"
+        f"    . $HOME/.local/venv_{ indicator_name }/bin/activate && \\\n"
+        f"    python3 $(ls -d $HOME/.local/venv_{ indicator_name }/lib/python3.* | head -1)/site-packages/{ indicator_name }/{ indicator_name }.py && \\\n"
+        f"    deactivate\n"
+        f"```\n\n" )
+
 
 
 def _get_distributions_tested():
