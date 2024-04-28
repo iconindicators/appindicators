eb96ff923aecf2b959ef99374311811b845324fe
bernmeister <thebernmeister@hotmail.com>
2024-04-28 23:35:19 +0000

    Neglected to rename the calls to __calculateCometsMinorPlanets() as __calculateComets() and __calculateMinorPlanets().

--- indicatorlunar/src/indicatorlunar/astropyephem.py
+++ indicatorlunar/src/indicatorlunar/astropyephem.py
@@ -189,17 +189,18 @@ class AstroPyEphem( AstroBase ):
         AstroPyEphem.__calculatePlanets( observer, data, planets, apparentMagnitudeMaximum )
         AstroPyEphem.__calculateStars( observer, data, stars, apparentMagnitudeMaximum )
 
-        AstroPyEphem.__calculateCometsMinorPlanets(
-            observer, data,
-            AstroBase.BodyType.COMET, comets, cometData, cometApparentMagnitudeData,
+        AstroPyEphem.__calculateComets(
+            observer,
+            data,
+            comets, cometData,
             apparentMagnitudeMaximum,
             logging )
 
-        AstroPyEphem.__calculateCometsMinorPlanets(
-            observer, data,
-            AstroBase.BodyType.MINOR_PLANET, minorPlanets, minorPlanetData, minorPlanetApparentMagnitudeData,
-            apparentMagnitudeMaximum,
-            logging )
+        AstroPyEphem.__calculateMinorPlanets(
+            observer,
+            data,
+            minorPlanets, minorPlanetData,
+            minorPlanetApparentMagnitudeData, apparentMagnitudeMaximum )
 
         AstroPyEphem.__calculateSatellites( ephemNow, observer, data, satellites, satelliteData, startHourAsDateTimeInUTC, endHourAsDateTimeInUTC )
 
