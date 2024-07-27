ef87f42ea52f12571f793e9255cfdd425e72b060
bernmeister <thebernmeister@hotmail.com>
2024-07-27 23:01:06 +0000

    In progress...

--- tools/build_locale.py
+++ tools/build_locale.py
@@ -21,79 +21,28 @@
 # I think the wording and some commands may be out of date.
 
 
-#TODO This is a new script...a copy of build_readme.py
-# Idea is to update POT and PO files as part of the build_wheel.py process...
-# But perhaps should be called to run over the src tree and NOT the release tree.
-# What is the point of updating the POT/PO files in the release and not the original src?
-#
-# Can pass some things to xgettext on the command line:
-#
-#   cd ~/Programming/Indicators/indicatorfortune/src/indicatorfortune
-#   xgettext --files-from=locale/POTFILES.in --copyright-holder="Bernard Giannetti" --package-name="indicatorfortune" --package-version="1.0.42" --msgid-bugs-address="<thebernmeister@hotmail.com>" --output=locale/POTFILES.pot && sed -i  's/SOME DESCRIPTIVE TITLE/Portable Object Template for indicatorfortune/' locale/POTFILES.pot && sed -i 's/YEAR Bernard Giannetti/2014-2024 Bernard Giannetti/' locale/indicatorfortune.pot && sed -i 's/CHARSET/UTF-8/' locale/indicatorfortune.pot
-#
-# which creates a new POT file and replaces text such as title, copyright year and charset.
-# Need to substitute all the above into a new script.
-#
+#TODO
 # To update each the .po file for each locale:
 #   cd ~/Programming/Indicators/indicatorfortune/src/indicatorfortune/locale
 #   msgmerge ru/LC_MESSAGES/indicatorfortune.po indicatorfortune.pot -o ru/LC_MESSAGES/indicatorfortune.po
 #
 # What about looking at the LINGUAS file;
-# if there is no po file for a given language, create it?
-# I don't think this is feasible...
-# ...there are too many pieces of text to swap out.
-# Perhaps the build POT/PO script could make the new PO file and leave it at that.
-# Or write a message saying there is no PO file for a given LINGUA entry?
+# if there is no po file for a given language, create it.
+# Or instead write a message saying there is no PO file for a given LINGUA entry?
 #
-# Still not sure when this script should be called...as part of the build_wheel
-# in the same way the build_readme is called?
-#
+#This script should be called as part of the build_wheel in the same way the build_readme.
 #
 # Once this script is sorted, maybe update the indicatorbase/locale/README
 # to say the README is a historical document.
 
 
-#TODO One thing to consider is that each time the build occurs and so running this script
-# is the pot/po files will be altered with at a minimum, new date/times.
-#
-# This is not enough to justify having to commit...
-# ...so is there a way to deal with this?
-# Can only think of when it is time to commit, having to revert each AND EVERY pot/po file...
-# pain in the neck!
-#
-# Maybe search in the file system for pot/po files and then can do a mass revert.
-# But still need a way to determine if a file differs only by date/time.
-#
-# Need to check/verify that doing the update of a POT or PO
-# where there are no changes from the source .py file
-# that the updated/generated POT/PO files are the same as the originals
-# and so that won't trigger a need to do a commit.
-
-
-# Create a README.md for an indicator from text common to all indicators and text
-# specific to the indicator, drawn from the indicator's CHANGELOG.md and pyproject.toml.
-#
-#   https://github.github.com/gfm
-#
-# When testing/editing, to render out in HTML:
-#   python3 -m pip install --upgrade readme_renderer readme_renderer[md]
-#   python3 tools/build_readme.py release indicatortest
-#   python3 -m readme_renderer release/README.md -o release/README.html
-#
-# References
-#   https://pycairo.readthedocs.io
-#   https://pygobject.readthedocs.io
-
-
 import argparse
 import datetime
-# import re
-import sys
 
-# import os
 from pathlib import Path
 
 import subprocess
+import sys
 
 
 #TODO I think this works in both eclipse and from terminal...
@@ -179,8 +128,7 @@ def _create_update_pot( indicator_name, project_metadata ):
 
     subprocess.run( command )
 
-    # If originally there was no POT file, a POT is created and job done.
-    # Otherwise, need to compare the old POT to the new POT...
+    # Create the POT if none exists; otherwise, compare the new POT with the original POT...
     if new_pot_file.endswith( ".new.pot" ):
         command = [
             f"sed",
