acba21be31d8de9890851616ac9311bf9fbdc8f7
bernmeister <thebernmeister@hotmail.com>
2024-09-18 23:51:01 +0000

    More on using a single venv.

--- indicatorbase/src/indicatorbase/platform/linux/post_install.sh
+++ indicatorbase/src/indicatorbase/platform/linux/post_install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 indicator_path=$(ls -d $HOME/.local/venv_indicators/lib/python3.* | head -1)/site-packages/{indicator_name}
-mkdir -p $HOME/.local/bin
-cp --remove-destination ${indicator_path}/platform/linux/{indicator_name}.sh $HOME/.local/bin
-mkdir -p $HOME/.local/share/applications
-cp --remove-destination ${indicator_path}/platform/linux/{indicator_name}.py.desktop $HOME/.local/share/applications
-mkdir -p $HOME/.local/share/icons/hicolor/scalable/apps
-cp --remove-destination ${indicator_path}/icons/*.svg $HOME/.local/share/icons/hicolor/scalable/apps
+
+bin_path=$HOME/.local/bin
+mkdir -p ${bin_path}
+cp --remove-destination ${indicator_path}/platform/linux/{indicator_name}.sh ${bin_path} 
+
+applications_path=$HOME/.local/share/applications
+mkdir -p ${applications_path}
+cp --remove-destination ${indicator_path}/platform/linux/{indicator_name}.py.desktop ${applications_path}
+
+icons_path=$HOME/.local/share/icons/hicolor/scalable/apps
+mkdir -p ${icons_path}
+cp --remove-destination ${indicator_path}/icons/*.svg ${icons_path}
--- indicatorbase/src/indicatorbase/platform/linux/run.sh
+++ indicatorbase/src/indicatorbase/platform/linux/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 venv=$HOME/.local/venv_indicators
-cd $HOME
+cd $HOME  #TODO Is this needed?
 . ${venv}/bin/activate
 python3 $(ls -d ${venv}/lib/python3.* | head -1)/site-packages/{indicator_name}/{indicator_name}.py
 deactivate
