e077714245fa28a294dd9c6182878d6dc1471e96
bernmeister <thebernmeister@hotmail.com>
2025-06-28 04:16:01 +0000

    Removed spaces from variables.

--- indicatorbase/src/indicatorbase/platform/linux/run.sh
+++ indicatorbase/src/indicatorbase/platform/linux/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 venv={ venv_indicators }
-. ${ venv }/bin/activate
-cd $(ls -d ${ venv }/lib/python3.* | head -1)/site-packages
+. ${venv}/bin/activate
+cd $(ls -d ${venv}/lib/python3.* | head -1)/site-packages
 python3 -m { indicator }.{ indicator }
 deactivate
 cd - > /dev/null
