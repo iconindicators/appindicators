9fec781d60776e5cfffcd7d278ed4184e1e84454
bernmeister <thebernmeister@hotmail.com>
2025-07-06 05:42:26 +0000

    Don't remove $HOME/.confg/{indiator} as it may contain information the user wants, such as scripts.

--- indicatorbase/src/indicatorbase/platform/linux/uninstall.sh
+++ indicatorbase/src/indicatorbase/platform/linux/uninstall.sh
@@ -4,4 +4,4 @@ rm -f $HOME/.local/bin/{indicator}.sh
 rm -f $HOME/.local/share/applications/{indicator}.py.desktop
 rm -f $HOME/.local/share/icons/hicolor/scalable/apps/{indicator}*.svg
 rm -f -r $HOME/.cache/{indicator}
-rm -f -r $HOME/.config/{indicator}
+# Keep $HOME/.config/{indicator} as it may contain information the user wants.
