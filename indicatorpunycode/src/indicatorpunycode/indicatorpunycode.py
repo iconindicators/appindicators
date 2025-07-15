a601bf2372cc6b234a4c51f1272cc6eccf245a98
bernmeister <thebernmeister@hotmail.com>
2025-07-15 09:26:18 +0000

    Added TODO.

--- indicatorpunycode/src/indicatorpunycode/indicatorpunycode.py
+++ indicatorpunycode/src/indicatorpunycode/indicatorpunycode.py
@@ -16,6 +16,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+#TODO Test on wayland.
+
+
 '''
 Application indicator which converts domain names between Unicode and ASCII.
 '''
