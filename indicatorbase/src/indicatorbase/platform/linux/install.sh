dde6ecba59e79a541e931d61d9520af2aa015293
bernmeister <thebernmeister@hotmail.com>
2025-10-21 11:05:25 +0000

    Take into account if the venv is specifiied to be a different directory other than the default (by the user in the install command).

--- indicatorbase/src/indicatorbase/platform/linux/install.sh
+++ indicatorbase/src/indicatorbase/platform/linux/install.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-indicator_path=$(ls -d {venv_indicators}/lib/python3.* | head -1)/site-packages/{indicator}
+
+# The full path to the virtual environment directory must be passed as the
+# first argument.  For example:
+#       $HOME/.local/venv_indicators
+
+
+indicator_path=$(ls -d $1/lib/python3.* | head -1)/site-packages/indicatortest
 
 bin_path=$HOME/.local/bin
 mkdir -p ${bin_path}
-cp --remove-destination ${indicator_path}/platform/linux/{indicator}.sh ${bin_path}
+cp --remove-destination ${indicator_path}/platform/linux/indicatortest.sh ${bin_path}
+sed -i "3s,^,venv=$1\n," ${bin_path}/indicatortest.sh
 
 applications_path=$HOME/.local/share/applications
 mkdir -p ${applications_path}
-cp --remove-destination ${indicator_path}/platform/linux/{indicator}.py.desktop ${applications_path}
+cp --remove-destination ${indicator_path}/platform/linux/indicatortest.py.desktop ${applications_path}
 
 icons_path=$HOME/.local/share/icons/hicolor/scalable/apps
 mkdir -p ${icons_path}
