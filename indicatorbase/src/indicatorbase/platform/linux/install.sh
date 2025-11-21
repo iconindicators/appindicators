811456edb2bf617c5f87d46180fc6736830e1dd8
Bernard Giannetti <thebernmeister@hotmail.com>
2025-11-21 11:43:57 +1100

    Erroneously was hardcoded for indicatortest.

--- indicatorbase/src/indicatorbase/platform/linux/install.sh
+++ indicatorbase/src/indicatorbase/platform/linux/install.sh
@@ -6,16 +6,16 @@
 #       $HOME/.local/venv_indicators
 
 
-indicator_path=$(ls -d $1/lib/python3.* | head -1)/site-packages/indicatortest
+indicator_path=$(ls -d $1/lib/python3.* | head -1)/site-packages/{indicator}
 
 bin_path=$HOME/.local/bin
 mkdir -p ${bin_path}
-cp --remove-destination ${indicator_path}/platform/linux/indicatortest.sh ${bin_path}
-sed -i "3s,^,venv=$1\n," ${bin_path}/indicatortest.sh
+cp --remove-destination ${indicator_path}/platform/linux/{indicator}.sh ${bin_path}
+sed -i "3s,^,venv=$1\n," ${bin_path}/{indicator}.sh
 
 applications_path=$HOME/.local/share/applications
 mkdir -p ${applications_path}
-cp --remove-destination ${indicator_path}/platform/linux/indicatortest.py.desktop ${applications_path}
+cp --remove-destination ${indicator_path}/platform/linux/{indicator}.py.desktop ${applications_path}
 
 icons_path=$HOME/.local/share/icons/hicolor/scalable/apps
 mkdir -p ${icons_path}
